bacb005f616d00b1cb221ab6cb993ea3e8d99cbb
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2024-12-21 11:52:43 +0530

    style: integrate Google sign-in option

--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -2,8 +2,10 @@
 import React, { useState } from "react";
 import { useAuth } from "../../../context/AuthContext";
 import { register } from "@/lib/api/authentication";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
+
+import { signIn } from "next-auth/react";
 
 const RegisterPage = () => {
   const [name, setName] = useState("");
@@ -13,7 +15,6 @@ const RegisterPage = () => {
   const [role, setRole] = useState("CANDIDATE");
   const { login } = useAuth();
   const router = useRouter();
-  // console.log(/[A-Z]/.test('1ssss'));
 
   const [isValidPassword, setIsValidPassword] = useState(false);
   const [isPasswordMissMatch, setIsPasswordMissMatch] = useState(false);
@@ -31,10 +32,10 @@ const RegisterPage = () => {
       setIsValidPassword(false);
       return true;
     }
-    
+
     setIsValidPassword(true);
-    return false
-  }
+    return false;
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -42,14 +43,14 @@ const RegisterPage = () => {
       if (passwordValidation(password, passwordconf)) {
         const userData = { name, email, password, role, passwordconf };
         const response = await register(userData);
-  
+
         if (response) {
           router.push("/login");
         }
-  
+
         // Log in the user after successful registration
         // login({ email: response.email, role: response.role, token: response.token });
-  
+
         // // Redirect based on role
         // if (response.role === 'ADMIN') {
         //   router.push('/admin/dashboard');
@@ -61,7 +62,7 @@ const RegisterPage = () => {
     } catch (err) {
       if (err.response) {
         const { data } = err.response;
-  
+
         if (data && data.message) {
           console.error("Registration failed:", data.message);
           alert(`Registration failed: ${data.message}`);
@@ -71,14 +72,25 @@ const RegisterPage = () => {
         }
       } else {
         console.error("An unexpected error occurred:", err);
-        alert("An unexpected error occurred. Please check your network and try again.");
+        alert(
+          "An unexpected error occurred. Please check your network and try again."
+        );
       }
     }
   };
 
+  const handleLoginSuccess = async () => {
+    signIn("google", {
+      // callbackUrl: `${
+      //   window.location.origin
+      // }/auth-callback?redirect=${encodeURIComponent(redirects)}`,
+      callbackUrl: "/login",
+    });
+  };
+
   return (
     <div className=" bg-background flex flex-col items-center justify-center h-lvh w-full">
-      <div className="h-fit w-[90%] md:w[50%] lg:w-[40%] max-w-[450px] bg-gradient-to-br from-[#1f2126] to-[#17191d] p-5 sm:p-7 md:p-8 rounded-3xl flex flex-col justify-center"> 
+      <div className="h-fit w-[90%] md:w[50%] lg:w-[40%] max-w-[450px] bg-gradient-to-br from-[#1f2126] to-[#17191d] p-5 sm:p-7 md:p-8 rounded-3xl flex flex-col justify-center">
         <h1 className=" text-2xl font-semibold text-[#f3f3f3]">Sign In</h1>
         <form
           onSubmit={handleSubmit}
@@ -107,19 +119,34 @@ const RegisterPage = () => {
             onChange={(e) => setPassword(e.target.value)}
             onClick={() => setIsValidPassword(false)}
             required
-            className={` h-[52px] w-full rounded-full text-base border-0 bg-[#32353b] placeholder-[#737883] px-6 py-2 mt-6 ${isValidPassword ? ' border-2 border-lightred' : ''}`}
+            className={` h-[52px] w-full rounded-full text-base border-0 bg-[#32353b] placeholder-[#737883] px-6 py-2 mt-6 ${
+              isValidPassword ? " border-2 border-lightred" : ""
+            }`}
           />
-          {isValidPassword && <span className=" text-lightred text-xs text-left w-full pt-1">* Password must contain at least one uppercase letter, one lowercase letter, one number, and be at least 6 characters long.</span>}
+          {isValidPassword && (
+            <span className=" text-lightred text-xs text-left w-full pt-1">
+              * Password must contain at least one uppercase letter, one
+              lowercase letter, one number, and be at least 6 characters long.
+            </span>
+          )}
           <input
             type="password"
             placeholder="Confirme Password"
             value={passwordconf}
             onChange={(e) => setPasswordconf(e.target.value)}
-            onClick={() => {setIsPasswordMissMatch(false)}}
+            onClick={() => {
+              setIsPasswordMissMatch(false);
+            }}
             required
-            className={` h-[52px] w-full rounded-full text-base border-0 bg-[#32353b] px-6 py-2 mt-6 appearance-none ${isPasswordMissMatch ? ' border-2 border-lightred' : ''}`}
+            className={` h-[52px] w-full rounded-full text-base border-0 bg-[#32353b] px-6 py-2 mt-6 appearance-none ${
+              isPasswordMissMatch ? " border-2 border-lightred" : ""
+            }`}
           />
-          {isPasswordMissMatch && <span className=" text-lightred text-xs text-left w-full pt-1">* Passwords do not match.</span>}
+          {isPasswordMissMatch && (
+            <span className=" text-lightred text-xs text-left w-full pt-1">
+              * Passwords do not match.
+            </span>
+          )}
           <select
             value={role}
             onChange={(e) => setRole(e.target.value)}
@@ -136,7 +163,15 @@ const RegisterPage = () => {
             Register
           </button>
         </form>
-        <div className="mt-8">
+        <div className=" flex items-center justify-center mt-3">
+          <img
+            src="https://th.bing.com/th/id/R.0fa3fe04edf6c0202970f2088edea9e7?rik=joOK76LOMJlBPw&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fgoogle-logo-png-open-2000.png&ehk=0PJJlqaIxYmJ9eOIp9mYVPA4KwkGo5Zob552JPltDMw%3d&risl=&pid=ImgRaw&r=0"
+            alt="google logo"
+            onClick={handleLoginSuccess}
+            className=" rounded-lg h-8 w-8"
+          />
+        </div>
+        <div className="mt-6">
           <p className=" text-[#808080] text-base text-center">
             Got an account?{" "}
             <Link
