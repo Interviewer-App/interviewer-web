0462573b0a3ec8523b3b140f47726e7c47f80956
ushansankalpa <ushansan99@gmail.com>
2025-01-03 12:41:28 +0530

    fixed issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`antialiased`}
+        className={`antialiased`} suppressHydrationWarning={true}
       >
         <AuthProvider>{children}</AuthProvider>
       </body>
