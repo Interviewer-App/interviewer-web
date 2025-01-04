35ad0fc8206b6f6d08feb2135ea2a8aa4893b12e
ushansankalpa <ushansan99@gmail.com>
2025-01-04 10:38:27 +0530

    added interview session

---
+++ src/app/(candidate)/interview-session/page.js
@@ -0,0 +1,13 @@
+
+import RecordingView from '@/components/RecordingView';
+import ProtectedRoute from '../../../components/shared/ProtectedRoute';
+
+const InterviewSession = () => {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center justify-between p-24'>
+        <RecordingView/> 
+    </div>
+  );
+};
+
+export default InterviewSession;
