6bba300057453ec639944cf16aae7a29f7f3ea1c
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-04 09:58:04 +0530

    Enhance theme configuration in Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
      './node_modules/flowbite/**/*.js'
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        lightred: "var(--lightred)",
-        darkred: "var(--darkred)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
+  			lightred: 'var(--lightred)',
+  			darkred: 'var(--darkred)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [
-    // require('flowbite/plugin')
-  ],
+      require("tailwindcss-animate")
+],
 };
