db0792a77269674f677676990520d384b80fb707
ushansankalpa <ushansan99@gmail.com>
2025-01-06 15:44:45 +0530

    remove borders

--- src/app/(candidate)/interview-session/page.js
+++ src/app/(candidate)/interview-session/page.js
@@ -32,10 +32,10 @@ const InterviewSession = () => {
   };
   return (
     <SidebarInset>
-                <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+                <header className="flex h-16 shrink-0 items-center gap-2">
                     <div className="flex items-center gap-2 px-3">
                       <SidebarTrigger />
-                      <Separator orientation="vertical" className="mr-2 h-4" />
+                      {/* <Separator orientation="vertical" className="mr-2 h-4" /> */}
                       <Breadcrumb>
                         <BreadcrumbList>
                           <BreadcrumbItem className="hidden md:block">
