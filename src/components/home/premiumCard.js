456ca467fbaf0cd8bb888c6ce974621632cfe322
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-06 12:42:39 +0530

    modify colors

--- src/components/home/premiumCard.js
+++ src/components/home/premiumCard.js
@@ -12,7 +12,7 @@ export default function premiumCard({
   duration,
 }) {
   return (
-    <div className={` rounded-lg bg-black/30 backdrop-blur-[6px] overflow-hidden border-2 ${type === 'Pro' ? 'border-red-400/30' : 'border-slate-500'} border-opacity-20 p-9 flex flex-col items-center h-full`}>
+    <div className={` rounded-lg bg-black/30 backdrop-blur-[6px] overflow-hidden border-2 ${type === 'Pro' ? 'border-[#81F0E8]/30' : 'border-slate-500'} border-opacity-20 p-9 flex flex-col items-center h-full`}>
       <div className=" flex justify-start items-center w-full">
         <div className=" bg-gradient-to-t from-black via-black to-slate-500/50 rounded-full p-[1px] mr-4">
           <div className=" h-10 aspect-square bg-background rounded-full flex justify-center items-center">
@@ -21,13 +21,13 @@ export default function premiumCard({
         </div>
         <p className=" text-xl font-semibold">{type}</p>
         {type === "Pro" && (
-          <div className=" absolute right-9 top-9 rounded-lg border-2 border-yellow-400/50 text-sm py-1 px-7 text-yellow-400/60">
+          <div className=" absolute right-9 top-9 rounded-lg border-2 border-[#81F0E8]/50 text-sm py-1 px-5 md:px-7 text-[#81F0E8]/60">
             Most popular
           </div>
         )}
       </div>
       <div className=" w-full mt-9">
-        <h1 className=" text-8xl font-semibold">${duration === "MONTHLY" ? priceM : priceY}</h1>
+        <h1 className=" text-6xl  md:text-8xl font-semibold">${duration === "MONTHLY" ? priceM : priceY}</h1>
         <p className=" text-sm font-semibold ">per user / {duration === "MONTHLY" ? 'month' : 'year'}</p>
       </div>
       <hr className=" w-full opacity-30 my-5" />
