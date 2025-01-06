133e7ee9fb7d489e389693b816a41873bbe73cb6
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-06 16:19:11 +0530

    feat: add interview schedule page component

---
+++ src/app/(candidate)/interview-schedules/page.js
@@ -0,0 +1,56 @@
+"use client";
+import InterviewScheduleCard from "@/components/interview-schedules/interview-schedule-card";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { FaPlus } from "react-icons/fa6";
+
+const InterviewSchedulePage = () => {
+  return (
+    <>
+      <SidebarInset>
+      <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+                    <div className="flex items-center gap-2 px-3">
+                      <SidebarTrigger />
+                      <Separator orientation="vertical" className="mr-2 h-4" />
+                      <Breadcrumb>
+                        <BreadcrumbList>
+                          <BreadcrumbItem className="hidden md:block">
+                            <BreadcrumbLink href="#">
+                              Candidate
+                            </BreadcrumbLink>
+                          </BreadcrumbItem>
+                          <BreadcrumbSeparator className="hidden md:block" />
+                          <BreadcrumbItem>
+                            <BreadcrumbPage>Interview Session</BreadcrumbPage>
+                          </BreadcrumbItem>
+                        </BreadcrumbList>
+                      </Breadcrumb>
+                    </div>
+                  </header>
+
+        <div className=" w-full p-9 h-full">
+          <h1 className=" text-4xl font-semibold">Scheduled Interviews</h1>
+          <div className=" grid grid-cols-1 gap-9 mt-6 md:grid-cols-2 lg:grid-cols-3">
+            <InterviewScheduleCard index="II" company="Company Name" date="10.02.2024" time="10.00 am"/>
+            <InterviewScheduleCard index="II" company="Company Name" date="10.02.2024" time="10.00 am"/>
+            <InterviewScheduleCard index="III" company="Company Name" date="10.02.2024" time="10.00 am"/>
+          </div>
+        </div>
+      </SidebarInset>
+    </>
+  );
+};
+
+export default InterviewSchedulePage;
