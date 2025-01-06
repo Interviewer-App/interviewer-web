6bed0c27d14bd793978412961fca2f984447bddf
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-06 16:19:22 +0530

    feat: enhance Interviews page layout and add InterviewDisplayCard component

--- src/app/(company)/interviews/page.js
+++ src/app/(company)/interviews/page.js
@@ -1,55 +1,59 @@
-'use client'
+"use client";
+import InterviewDisplayCard from "@/components/interviews/interview-display-card";
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-  } from "@/components/ui/breadcrumb";
-  import { Separator } from "@/components/ui/separator";
-  import {
-    SidebarInset,
-    SidebarProvider,
-    SidebarTrigger,
-  } from "@/components/ui/sidebar";
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { FaPlus } from "react-icons/fa6";
 
 const InterviewsPage = () => {
-    return (
-          <>
-          <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-                <div className="flex items-center gap-2 px-3">
-                  <SidebarTrigger />
-                  <Separator orientation="vertical" className="mr-2 h-4" />
-                  <Breadcrumb>
-                    <BreadcrumbList>
-                      <BreadcrumbItem className="hidden md:block">
-                        <BreadcrumbLink href="#">
-                          Company
-                        </BreadcrumbLink>
-                      </BreadcrumbItem>
-                      <BreadcrumbSeparator className="hidden md:block" />
-                      <BreadcrumbItem>
-                        <BreadcrumbPage>Interviews</BreadcrumbPage>
-                      </BreadcrumbItem>
-                    </BreadcrumbList>
-                  </Breadcrumb>
-                </div>
-              </header>
-      
-              <div className="flex flex-1 flex-col gap-4 p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                  <h1>AIII </h1>
-                  <div className="aspect-video rounded-xl bg-muted/50" />
-                  <div className="aspect-video rounded-xl bg-muted/50" />
-                  <div className="aspect-video rounded-xl bg-muted/50" />
-                </div>
-                <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+  return (
+    <>
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+          <div className="flex items-center gap-2 px-3">
+            <SidebarTrigger />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">Company</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Interviews</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+
+        <div className=" w-full p-9 h-full">
+          <h1 className=" text-4xl font-semibold">Interviews</h1>
+          <div className=" grid grid-cols-1 gap-9 mt-6 md:grid-cols-2 lg:grid-cols-3">
+            <div className=" w-full h-full cursor-pointer border-4 border-dashed border-gray-700 flex flex-col items-center justify-center rounded-xl">
+              <div className=" bg-slate-500 rounded-full p-6 m-14 md:m-0">
+                <FaPlus className=" text-white text-3xl " />
               </div>
-              </SidebarInset>
-          </>
-    );
-  };
-  
-  export default InterviewsPage;
+            </div>
+            <InterviewDisplayCard index="I" candidate="Candidate Name" date="10.02.2024" time="10.00 am"/>
+            <InterviewDisplayCard index="I" candidate="Candidate Name" date="10.02.2024" time="10.00 am"/>
+            <InterviewDisplayCard index="I" candidate="Candidate Name" date="10.02.2024" time="10.00 am"/>
+          </div>
+        </div>
+      </SidebarInset>
+    </>
+  );
+};
+
+export default InterviewsPage;
