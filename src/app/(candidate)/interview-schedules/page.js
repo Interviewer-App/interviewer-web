e9c13255d08b821dcf1af6cd606a34fe2267d929
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-07 11:46:11 +0530

    feat: fetch and display interviews in InterviewSchedulePage component

--- src/app/(candidate)/interview-schedules/page.js
+++ src/app/(candidate)/interview-schedules/page.js
@@ -14,38 +14,58 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar";
+import { getAllInterviews } from "@/lib/api/interview";
+import { useEffect, useState } from "react";
 import { FaPlus } from "react-icons/fa6";
 
 const InterviewSchedulePage = () => {
+  const [interviews, setInterviews] = useState([]);
+
+  useEffect(() => {
+    const fetchAllInterviews = async () => {
+      try {
+        const response = await getAllInterviews();
+        setInterviews(response.data);
+      } catch (error) {
+        console.log("Error fetching interviews:", error);
+      }
+    };
+    fetchAllInterviews();
+  }, []);
+
   return (
     <>
       <SidebarInset>
-      <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-                    <div className="flex items-center gap-2 px-3">
-                      <SidebarTrigger />
-                      <Separator orientation="vertical" className="mr-2 h-4" />
-                      <Breadcrumb>
-                        <BreadcrumbList>
-                          <BreadcrumbItem className="hidden md:block">
-                            <BreadcrumbLink href="#">
-                              Candidate
-                            </BreadcrumbLink>
-                          </BreadcrumbItem>
-                          <BreadcrumbSeparator className="hidden md:block" />
-                          <BreadcrumbItem>
-                            <BreadcrumbPage>Interview Session</BreadcrumbPage>
-                          </BreadcrumbItem>
-                        </BreadcrumbList>
-                      </Breadcrumb>
-                    </div>
-                  </header>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+          <div className="flex items-center gap-2 px-3">
+            <SidebarTrigger />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">Candidate</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Interview Session</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
 
         <div className=" w-full p-9 h-full">
           <h1 className=" text-4xl font-semibold">Scheduled Interviews</h1>
           <div className=" grid grid-cols-1 gap-9 mt-6 md:grid-cols-2 lg:grid-cols-3">
-            <InterviewScheduleCard index="II" company="Company Name" date="10.02.2024" time="10.00 am"/>
-            <InterviewScheduleCard index="II" company="Company Name" date="10.02.2024" time="10.00 am"/>
-            <InterviewScheduleCard index="III" company="Company Name" date="10.02.2024" time="10.00 am"/>
+            {interviews.map((interview, index) => (
+              <InterviewScheduleCard
+                key={index}
+                index={index+1}
+                title={interview.jobTitle}
+                date={interview.scheduledDate}
+                time={interview.scheduledAt}
+              />
+            ))}
           </div>
         </div>
       </SidebarInset>
