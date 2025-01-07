e62d3265fd26f3e5e8b767760ddefa82e16bae9a
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-07 17:15:27 +0530

    refactor: update props in InterviewScheduleCard

--- src/components/interview-schedules/interview-schedule-card.js
+++ src/components/interview-schedules/interview-schedule-card.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-export default function InterviewScheduleCard({ index, title, date, time }) {
+export default function InterviewScheduleCard({ index, interview}) {
   return (
     <div className=" relative w-full h-full flex flex-col items-center justify-center rounded-xl p-6 bg-gray-800/80">
       <h1 className=" text-lg">Interview {index}</h1>
       <h3 className=" text-2xl lg:text-4xl font-semibold py-3 text-center">
-        {title}
+        {interview.jobTitle}
       </h3>
-      <h3 className=" text-sm md:text-base">Date: {new Date(date).toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' })}</h3>
-      <h3 className=" text-sm md:text-base">Time: {new Date(time).toLocaleTimeString()}</h3>
+      <h3 className=" text-sm md:text-base">Date: {new Date(interview.scheduledDate).toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' })}</h3>
+      <h3 className=" text-sm md:text-base">Time: {new Date(interview.scheduledAt).toLocaleTimeString()}</h3>
       <button
         type="submit"
         className=" h-12 min-w-[150px] w-full md:w-[40%] mt-8 cursor-pointer bg-gradient-to-b from-lightred to-darkred rounded-lg text-center text-base text-white font-semibold"
