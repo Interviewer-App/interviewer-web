ed1b36309cc68807a65da9c96c04afca058ba4b4
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-07 16:53:11 +0530

    feat: add interview fetching and updating functions

--- src/lib/api/interview.js
+++ src/lib/api/interview.js
@@ -20,12 +20,42 @@ export const createInterview = async (data) => {
     }
 }
 
+export const getPublishedInterview = async () => {
+    try{
+        const response = await axiosInstance.get(`/interview/published`);
+        return response;
+    } catch(error){
+        console.log('Error fetching published interviews:', error);
+        throw error;
+    }
+}
+
 export const getAllInterviews = async () => {
     try{
         const response = await axiosInstance.get(`/interview`);
         return response;
     } catch(error){
         console.log('Error fetching all interviews:', error);
+        throw error;
+    }
+}
+
+export const updateInterview = async (interviewId, data) => {
+    try{
+        const response = await axiosInstance.patch(`/interview/${interviewId}`, data);
+        return response;
+    } catch(error){
+        console.log('Error updating interview:', error);
+        throw error;
+    }
+}
+
+export const getInterviewById = async (interviewId) => {
+    try{
+        const response = await axiosInstance.get(`/interview/interview/${interviewId}`);
+        return response;
+    } catch(error){
+        console.log('Error fetching interview:', error);
         throw error;
     }
 }
