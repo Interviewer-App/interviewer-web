1f16a52bbff55f55ba1b06c28982e7475aab7186
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-08 09:56:02 +0530

    refactor: update interview fetching function with company id

--- src/app/(company)/interviews/page.js
+++ src/app/(company)/interviews/page.js
@@ -19,17 +19,30 @@ import { use, useEffect, useState } from "react";
 import { FaPlus } from "react-icons/fa6";
 import { getInterviews } from "@/lib/api/interview";
 
+//MUI
+import { useToast } from "@/hooks/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+import { getSession } from "next-auth/react";
+
 const InterviewsPage = () => {
   const [interviews, setInterviews] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
+  const { toast } = useToast();
 
   useEffect(() => {
     const fetchInterviews = async () => {
       try {
-        const response = await getInterviews("cm5f3mptx0001u77ka8iv6sss");
+        const session = await getSession();
+        const companyId = session?.user?.companyID;
+        const response = await getInterviews(companyId);
         setInterviews(response.data);
       } catch (error) {
-        console.log("Error fetching interviews:", error);
+        toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description: `Error fetching interviews: ${error}`,
+          action: <ToastAction altText="Try again">Try again</ToastAction>,
+        });
       }
     };
     fetchInterviews();
@@ -59,7 +72,10 @@ const InterviewsPage = () => {
         <div className=" w-full p-9 h-full">
           <h1 className=" text-4xl font-semibold">Interviews</h1>
           <div className=" grid grid-cols-1 gap-9 mt-6 md:grid-cols-2 lg:grid-cols-3">
-            <div onClick={() => setModalOpen(true)} className=" w-full h-full cursor-pointer border-4 border-dashed border-gray-700 flex flex-col items-center justify-center rounded-xl">
+            <div
+              onClick={() => setModalOpen(true)}
+              className=" w-full h-full cursor-pointer border-4 border-dashed border-gray-700 flex flex-col items-center justify-center rounded-xl"
+            >
               <div className=" bg-slate-500 rounded-full p-6 m-14 md:m-0">
                 <FaPlus className=" text-white text-3xl " />
               </div>
@@ -67,13 +83,13 @@ const InterviewsPage = () => {
             {interviews.map((interview, index) => (
               <InterviewDisplayCard
                 key={index}
-                index={index+1}
+                index={index + 1}
                 interview={interview}
               />
             ))}
           </div>
         </div>
-        {modalOpen && (<CreateInterviewModal setModalOpen={setModalOpen}/>)}
+        {modalOpen && <CreateInterviewModal setModalOpen={setModalOpen} />}
       </SidebarInset>
     </>
   );
