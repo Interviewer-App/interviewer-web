68bc20711a4df94ab41503ab90c0b8d6f067c244
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-09 14:59:47 +0530

    fix: update button width

--- src/components/ui/InterviewDataTable/Datatable.js
+++ src/components/ui/InterviewDataTable/Datatable.js
@@ -63,7 +63,7 @@ export function DataTable({ columns, data }) {
         />
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto">
+            <Button variant="outline" className="max-w-[120px] ml-auto">
               Columns
             </Button>
           </DropdownMenuTrigger>
