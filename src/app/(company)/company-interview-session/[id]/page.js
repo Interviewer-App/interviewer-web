3131f571cb5e37c9473ec309e395b25e495b9e42
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-10 12:34:47 +0530

    feat: enhance InterviewSessionPreviewPage with improved UI and question management

--- src/app/(company)/company-interview-session/[id]/page.js
+++ src/app/(company)/company-interview-session/[id]/page.js
@@ -1,14 +1,30 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import { useToast } from "@/hooks/use-toast";
+
+//UI Components
+import { ToastAction } from "@/components/ui/toast";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import { Separator } from "@radix-ui/react-dropdown-menu";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+//API
 import { getInterviewSessionById } from "@/lib/api/interview-session";
 import { generateQuestions } from "@/lib/api/ai";
-import { useToast } from "@/hooks/use-toast";
-import { ToastAction } from "@/components/ui/toast";
+import QuestionDisplayCard from "@/components/company/question-display-card";
 
 function InterviewSessionPreviewPage({ params }) {
   const [sessionId, setSessionId] = useState(null);
   const [isGenerateQuestions, setGenerateQuestions] = useState(false);
   const [sessionDetails, setSessionDetails] = useState({});
+  const [isQuestionEdit, setIsQuestionEdit] = useState(false);
   const { toast } = useToast();
 
   useEffect(() => {
@@ -38,7 +54,7 @@ function InterviewSessionPreviewPage({ params }) {
     };
 
     fetchSessionDetails();
-  }, [sessionId, isGenerateQuestions]);
+  }, [sessionId, isGenerateQuestions, isQuestionEdit]);
 
   const handleQuestionGenarate = async (e) => {
     e.preventDefault();
@@ -50,9 +66,9 @@ function InterviewSessionPreviewPage({ params }) {
         skillLevel: "Senior",
       });
 
-        if (response) {
-          setGenerateQuestions(true);
-        }
+      if (response) {
+        setGenerateQuestions(true);
+      }
     } catch (error) {
       if (error.response) {
         const { data } = error.response;
@@ -61,7 +77,7 @@ function InterviewSessionPreviewPage({ params }) {
           toast({
             variant: "destructive",
             title: "Uh oh! Something went wrong.",
-            description: `Interview update failed: ${data.message}`,
+            description: `Question Generation failed: ${data.message}`,
             action: <ToastAction altText="Try again">Try again</ToastAction>,
           });
         } else {
@@ -84,26 +100,84 @@ function InterviewSessionPreviewPage({ params }) {
     }
   };
   return (
-    <div>
-      <h1>Interview Session Preview</h1>
-      <p>Session ID: {sessionId}</p>
-      {sessionDetails.questions?.length > 0 ? (
-        sessionDetails.questions.map((question, index) => (
-          <div key={index}>
-            <h1 className=" text-xl font-semibold mt-5">
-              question {index + 1}
-            </h1>
-            <p>{question.questionText}</p>
+    <>
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2">
+          <div className="flex items-center gap-2 px-3">
+            <SidebarTrigger />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">Company</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Session Preview</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
           </div>
-        ))
-      ) : (
-        <p>No questions available.</p>
-      )}
+        </header>
+        <div className="px-9 py-4  max-w-[1500px] w-full mx-auto">
+          <h1 className=" text-4xl font-semibold">Session Preview</h1>
+          <div className=" flex flex-col md:flex-row items-center justify-start md:justify-between mt-5 w-full bg-gray-500/20 rounded-lg p-5">
+            <div className=" w-full md:w-[50%] ">
+              <h1 className=" text-3xl font-semibold">
+                {sessionDetails?.interview?.jobTitle || ""} Position
+              </h1>
+              <h1 className=" text-xl font-semibold text-gray-500">
+                {sessionDetails?.interviewCategory || ""}  Interview
+              </h1>
+              <p className=" text-base pt-3 text-gray-400">
+                Scheduled Date:{" "}
+                {new Date(
+                  sessionDetails?.interview?.scheduledDate
+                ).toLocaleDateString() || ""}
+              </p>
+              <p className=" text-base pt-1 text-gray-400">
+                Scheduled Time:{" "}
+                {new Date(
+                  sessionDetails?.interview?.scheduledAt
+                ).toLocaleTimeString() || ""}
+              </p>
+            </div>
+            <div className=" w-full md:w-[50%] flex items-center justify-start md:justify-end mt-5 md:mt-0">
+            <button
+                className=" h-12 min-w-[150px] w-[280px] cursor-pointer bg-gradient-to-b from-lightred to-darkred rounded-lg text-center text-base text-white font-semibold"
+              >
+                Start Interview
+              </button>
+            </div>
+          </div>
 
-      <div>
-        <button onClick={handleQuestionGenarate}>Genarate questions</button>
-      </div>
-    </div>
+          <div className="mt-5 bg-slate-500/10 p-5 rounded-lg">
+            <h1 className=" text-2xl font-semibold">Questions</h1>
+            {sessionDetails.questions?.length > 0 ? (
+              sessionDetails.questions.map((question, index) => (
+                <QuestionDisplayCard
+                  key={index}
+                  index={index}
+                  question={question}
+                  isQuestionEdit={isQuestionEdit}
+                  setIsQuestionEdit={setIsQuestionEdit}
+                />
+              ))
+            ) : (
+              <p>No questions available.</p>
+            )}
+            <div className=" w-full flex items-center justify-center">
+              <button
+                className=" h-12 min-w-[150px] w-[280px] mt-8 cursor-pointer bg-gradient-to-b from-lightred to-darkred rounded-lg text-center text-base text-white font-semibold"
+                onClick={handleQuestionGenarate}
+              >
+                Genarate questions
+              </button>
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </>
   );
 }
 
