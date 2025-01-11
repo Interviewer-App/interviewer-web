d0052d81998f9c21e85e80917e2f92566da1a6cf
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-11 17:35:10 +0530

    feat: add analyzeQuestion function for AI question analysis

--- src/lib/api/ai.js
+++ src/lib/api/ai.js
@@ -1,11 +1,22 @@
 import axiosInstance from "./axioinstance";
 
 export const generateQuestions = async (sessionId, data) => {
-    try{
-        const response = await axiosInstance.post(`/ai/generate-questions/${sessionId}`, data);
-        return response;
-    } catch(error){
-        console.log('Error generating questions:', error);
-        throw error;
-    }
-}
+  try {
+    const response = await axiosInstance.post(`/ai/generate-questions/${sessionId}`, data
+    );
+    return response;
+  } catch (error) {
+    console.log("Error generating questions:", error);
+    throw error;
+  }
+};
+
+export const analiyzeQuestion = async (data) => {
+  try {
+    const response = await axiosInstance.post(`/ai/analiyze-question`, data);
+    return response;
+  } catch (error) {
+    console.log("Error analiyzing questions:", error);
+    throw error;
+  }
+};
