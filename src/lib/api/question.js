9fa0654c9b82dc7b43da8744ef9368f962be7032
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-13 16:42:47 +0530

    fix: add createQuestion method

--- src/lib/api/question.js
+++ src/lib/api/question.js
@@ -5,7 +5,17 @@ export const updateQuestion = async (questionId, data) => {
         const response = await axiosInstance.patch(`/interview-session/question/${questionId}`, data);
         return response;
     } catch(error){
-        console.log('Error updating interview:', error);
+        console.log('Error updating question:', error);
+        throw error;
+    }
+}
+
+export const createQuestion = async (data) => {
+    try{
+        const response = await axiosInstance.post(`/interview-session/question/`, data);
+        return response;
+    } catch(error){
+        console.log('Error creating question:', error);
         throw error;
     }
 }
@@ -15,7 +25,7 @@ export const deleteQuestion = async (questionId) => {
         const response = await axiosInstance.delete(`/interview-session/question/${questionId}`);
         return response;
     } catch(error){
-        console.log('Error deleting interview:', error);
+        console.log('Error deleting question:', error);
         throw error;
     }
 }
