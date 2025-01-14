f19a33b74d6595751ca82b0c0fb386428b31a9ac
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-14 12:05:32 +0530

    refactor: improve code readability and formatting in interview session API

--- src/lib/api/interview-session.js
+++ src/lib/api/interview-session.js
@@ -1,31 +1,39 @@
 import axiosInstance from "./axioinstance";
 
 // api function to fetch joined interviews
-const fetchJoinedInterviews = async (candidateId, page, limit, setLoading, setPayments, setTotalUsers) => {
+const fetchJoinedInterviews = async (
+  candidateId,
+  page,
+  limit,
+  setLoading,
+  setPayments,
+  setTotalUsers
+) => {
   setLoading(true); // Start loading
   try {
-      const response = await axiosInstance.get(`/interview-session/candidate/${candidateId}/${page}/${limit}`);
-      console.log(response); // Ensure you're logging the correct structure
+    const response = await axiosInstance.get(
+      `/interview-session/candidate/${candidateId}/${page}/${limit}`
+    );
+    console.log(response); // Ensure you're logging the correct structure
 
-      if (!response || !response.data || !response.data.interviewSessions) {
-          throw new Error(`Error fetching interviews: No data found`);
-      }
+    if (!response || !response.data || !response.data.interviewSessions) {
+      throw new Error(`Error fetching interviews: No data found`);
+    }
 
-      const data = response.data.interviewSessions; // Access the user data here
-      const totalUsers = response.data.total; // Access the total users count
-      if (data && data.length > 0) {
-          setPayments(data);  // Update state for payments (interviews data)
-          setTotalUsers(totalUsers); // Update total users
-      } else {
-          console.log("No interviews found.");
-      }
+    const data = response.data.interviewSessions; // Access the user data here
+    const totalUsers = response.data.total; // Access the total users count
+    if (data && data.length > 0) {
+      setPayments(data); // Update state for payments (interviews data)
+      setTotalUsers(totalUsers); // Update total users
+    } else {
+      console.log("No interviews found.");
+    }
   } catch (error) {
-      console.log("Error fetching interviews:", error);
+    console.log("Error fetching interviews:", error);
   } finally {
-      setLoading(false); 
+    setLoading(false);
   }
 };
-
 
 const fetchInterviewSessionsForInterview = async (
   interviewId,
@@ -61,34 +69,42 @@ const fetchInterviewSessionsForInterview = async (
 };
 
 const getInterviewSessionById = async (sessionId) => {
-  try{
-      const response = await axiosInstance.get(`/interview-session/session/${sessionId}`);
-      return response;
-  } catch(error){
-      console.log('Error fetching session:', error);
-      throw error;
+  try {
+    const response = await axiosInstance.get(
+      `/interview-session/session/${sessionId}`
+    );
+    return response;
+  } catch (error) {
+    console.log("Error fetching session:", error);
+    throw error;
   }
-}
+};
 
 const getInterviewSessionHistoryById = async (sessionId) => {
-  try{
-      const response = await axiosInstance.get(`/interview-session/session-history/${sessionId}`);
-      return response;
-  } catch(error){
-      console.log('Error fetching session history:', error);
-      throw error;
+  try {
+    const response = await axiosInstance.get(
+      `/interview-session/session-history/${sessionId}`
+    );
+    return response;
+  } catch (error) {
+    console.log("Error fetching session history:", error);
+    throw error;
   }
-}
+};
 
 export const createInterviewSession = async (data) => {
-  try{
-      const response = await axiosInstance.post(`/interview-session`, data);
-      return response;
-  } catch(error){
-      console.log('Error Create interview session:', error);
-      throw error;
+  try {
+    const response = await axiosInstance.post(`/interview-session`, data);
+    return response;
+  } catch (error) {
+    console.log("Error Create interview session:", error);
+    throw error;
   }
-}
+};
 
-
-export { fetchJoinedInterviews, fetchInterviewSessionsForInterview, getInterviewSessionById, getInterviewSessionHistoryById };
+export {
+  fetchJoinedInterviews,
+  fetchInterviewSessionsForInterview,
+  getInterviewSessionById,
+  getInterviewSessionHistoryById,
+};
