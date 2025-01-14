3c85810c3f978aa8f7d9a462bfcf5ba98de69b42
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-14 17:27:11 +0530

    feat: add filtering functionality to InterviewSchedulePage

--- src/app/(candidate)/interview-schedules/page.js
+++ src/app/(candidate)/interview-schedules/page.js
@@ -8,6 +8,17 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+
 import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
@@ -24,19 +35,19 @@ import Image from "next/image";
 const InterviewSchedulePage = () => {
   const [interviews, setInterviews] = useState([]);
   const [isAnyInterviews, setIsAnyInterviews] = useState(false);
+  const [sordBy, setSortBy] = useState("latest");
+  const [datePosted, setDatePosted] = useState("last 24 hours");
+  const [interviewCategory, setInterviewCategory] = useState("Technical");
+  const [jobTitle, setJobTitle] = useState("");
+  const [keyWords, setKeyWords] = useState("");
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
+
+  const handleOpen = () => setIsSheetOpen(true);
+  const handleClose = () => setIsSheetOpen(false);
 
   useEffect(() => {
-    const fetchPublishedInterviews = async () => {
-      try {
-        const response = await getPublishedInterview();
-        setInterviews(response.data);
-        setIsAnyInterviews(response.data.length > 0);
-      } catch (error) {
-        console.log("Error fetching interviews:", error);
-      }
-    };
-
     socket.on("published", (data) => {
+      debugger;
       fetchPublishedInterviews();
     });
 
@@ -45,8 +56,30 @@ const InterviewSchedulePage = () => {
     return () => {
       socket.off("published");
     };
-
   }, []);
+
+  const fetchPublishedInterviews = async () => {
+    try {
+      console.log("interviewCategory", interviewCategory);
+      const response = await getPublishedInterview(
+        sordBy,
+        datePosted,
+        interviewCategory,
+        jobTitle,
+        keyWords
+      );
+      setInterviews(response.data);
+      setIsAnyInterviews(response.data.length > 0);
+    } catch (error) {
+      console.log("Error fetching interviews:", error);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchPublishedInterviews();
+    setIsSheetOpen(false);
+  };
 
   return (
     <>
@@ -69,8 +102,138 @@ const InterviewSchedulePage = () => {
           </div>
         </header>
 
-        <div className=" px-9 py-4 w-full max-w-[1500px] mx-auto h-full text-white">
-          <h1 className=" text-4xl font-semibold">Scheduled Interviews</h1>
+        <div className=" w-full p-9 h-full text-white">
+          <div className=" w-full flex justify-between items-center">
+            <h1 className=" text-4xl font-semibold">Scheduled Interviews</h1>
+            <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
+              <SheetTrigger>
+                <span className=" bg-transparent border-2 border-gray-500 text-gray-500 hover:text-gray-300 hover:border-gray-300 px-5 py-2 rounded-sm cursor-pointer">
+                  Apply Filter
+                </span>
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>
+                    <span className=" text-2xl font-semibold">
+                      Filter Interviews by
+                    </span>
+                  </SheetTitle>
+                  {/* <SheetDescription>
+                    This action cannot be undone. This will permanently delete
+                    your account and remove your data from our servers.
+                  </SheetDescription> */}
+                </SheetHeader>
+                <form onSubmit={handleSubmit}>
+                  <div className=" w-full mt-5">
+                    <span className=" text-xl font-semibold text-gray-400">
+                      Sort by
+                    </span>
+                    <RadioGroup defaultValue={sordBy} onValueChange={setSortBy}>
+                      <div className=" w-full flex space-x-8 mt-5 px-5">
+                        <div className="flex items-center space-x-2">
+                          <RadioGroupItem value="latest" id="latest" />
+                          <Label htmlFor="latest">Latest</Label>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                          <RadioGroupItem value="a-z" id="Ascending" />
+                          <Label htmlFor="Ascending">Ascending</Label>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                          <RadioGroupItem value="z-a" id="Descending" />
+                          <Label htmlFor="Descending">Descending</Label>
+                        </div>
+                      </div>
+                    </RadioGroup>
+                  </div>
+                  <div className=" w-full mt-6">
+                    <span className=" text-xl font-semibold text-gray-400">
+                      Date posted
+                    </span>
+                    <RadioGroup
+                      defaultValue={datePosted}
+                      onValueChange={setDatePosted}
+                    >
+                      <div className=" w-full flex space-x-8 mt-5 px-5">
+                        <div className="flex items-center space-x-2">
+                          <RadioGroupItem
+                            value="last 24 hours"
+                            id="Ascending"
+                          />
+                          <Label htmlFor="Ascending">Past 24 hours</Label>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                          <RadioGroupItem value="last week" id="last week" />
+                          <Label htmlFor="last week">Past week</Label>
+                        </div>
+                      </div>
+                      <div className=" w-full flex space-x-8 mt-3 px-5">
+                        <div className="flex items-center space-x-2">
+                          <RadioGroupItem value="last month" id="last month" />
+                          <Label htmlFor="last month">Past month</Label>
+                        </div>
+                      </div>
+                    </RadioGroup>
+                  </div>
+                  <div className=" w-full mt-6">
+                    <span className=" text-xl font-semibold text-gray-400">
+                      Interview catorgory
+                    </span>
+                    <RadioGroup
+                      defaultValue={interviewCategory}
+                      onValueChange={setInterviewCategory}
+                    >
+                      <div className=" w-full flex space-x-8 mt-5 px-5">
+                        <div className="flex items-center space-x-2">
+                          <RadioGroupItem value="Technical" id="Technical" />
+                          <Label htmlFor="Technical">Technical</Label>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                          <RadioGroupItem value="Behavioral" id="Behavioral" />
+                          <Label htmlFor="Behavioral">Behavioral</Label>
+                        </div>
+                      </div>
+                    </RadioGroup>
+                  </div>
+                  <div className=" w-full mt-6">
+                    <span className=" text-xl font-semibold text-gray-400">
+                      Job Title
+                    </span>
+                    <input
+                      typr="text"
+                      placeholder="Job Title"
+                      value={jobTitle}
+                      onChange={(e) => setJobTitle(e.target.value)}
+                      className=" mt-5 h-[45px] w-full rounded-lg text-sm border-0 bg-[#32353b] placeholder-[#737883] px-6 py-2"
+                    />
+                  </div>
+                  <div className=" w-full mt-6">
+                    <span className=" text-xl font-semibold text-gray-400">
+                      Key words
+                    </span>
+
+                    <input
+                      typr="text"
+                      placeholder="Key words"
+                      value={keyWords}
+                      onChange={(e) => setKeyWords(e.target.value)}
+                      className=" mt-5 h-[45px] w-full rounded-lg text-sm border-0 bg-[#32353b] placeholder-[#737883] px-6 py-2"
+                    />
+                  </div>
+                  <div className=" w-full flex justify-between mt-5">
+                    <button className=" mt-6 bg-transparent border-2 hover:text-white hover:border-white border-gray-600 rounded-lg text-center text-base text-gray-600 font-semibold h-12 w-[150px]">
+                      Reset fitler
+                    </button>
+                    <button
+                      type="submit"
+                      className=" mt-6 bg-gradient-to-b from-lightred to-darkred rounded-lg text-center text-sm text-white font-semibold h-12 w-[150px]"
+                    >
+                      Apply Filter
+                    </button>
+                  </div>
+                </form>
+              </SheetContent>
+            </Sheet>
+          </div>
           {isAnyInterviews ? (
             <div className=" grid grid-cols-1 gap-9 mt-6 md:grid-cols-2 lg:grid-cols-3">
               {interviews.map((interview, index) => (
