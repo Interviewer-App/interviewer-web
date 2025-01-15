5ce35332fcb19bf3a62c51a431569a33f0481c5f
Madhawa Awishka <madhawaawishka@gmail.com>
2025-01-15 16:09:05 +0530

    feat:change the appsidebar icons in the company section

--- src/components/app-sidebar.js
+++ src/components/app-sidebar.js
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Calendar, Home, Inbox, Search, Settings,Activity,Atom  } from "lucide-react";
+import { Calendar, Home, Inbox,Activity, Atom, Timer, UserPlus} from "lucide-react";
 import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation"; //use next/navigation insted of next/router(ref:stackoverflow)
 import {
@@ -66,12 +66,12 @@ const candidateItems = [
   {
     title: "Interview scheduls",
     url: "/interview-schedules",
-    icon: Search,
+    icon: Timer,
   },
   {
     title: "Joined Interviews",
     url: "/joined-interviews",
-    icon: Activity ,
+    icon: UserPlus,
   },
 ];
 const adminItems = [
