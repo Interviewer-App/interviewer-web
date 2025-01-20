8c5b85e16f124b84ce60b9ccce9f387274bc9b55
Madhawa Awishka <madhawaawishka@gmail.com>
2025-01-20 17:33:45 +0530

    feat:Adding card component

--- src/app/(candidate)/my-interviews/page.js
+++ src/app/(candidate)/my-interviews/page.js
@@ -18,6 +18,25 @@ import {
 import Loading from "@/app/loading";
 import { usePathname, useRouter, redirect } from 'next/navigation';
 import { useSession, getSession } from "next-auth/react"
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+
 
 
 const MyInterviews = () => {
@@ -106,15 +125,52 @@ const MyInterviews = () => {
                 <h1 className="text-3xl font-semibold">My Interviews</h1>
                 <div className=" bg-slate-600/10 w-full h-fit  p-9 rounded-lg mt-5">
                     <div>
-                        <h1 className=" text-2xl font-semibold">My Interview</h1>
+                        <h1 className=" text-2xl font-semibold mb-4">My Interview</h1>
+                        <Card className="w-[350px]">
+                            <CardHeader>
+                                <CardTitle>Create project</CardTitle>
+                                <CardDescription>Deploy your new project in one-click.</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <form>
+                                    <div className="grid w-full items-center gap-4">
+                                        <div className="flex flex-col space-y-1.5">
+                                            <Label htmlFor="name">Name</Label>
+                                            <Input id="name" placeholder="Name of your project" />
+                                        </div>
+                                        <div className="flex flex-col space-y-1.5">
+                                            <Label htmlFor="framework">Framework</Label>
+                                            <Select>
+                                                <SelectTrigger id="framework">
+                                                    <SelectValue placeholder="Select" />
+                                                </SelectTrigger>
+                                                <SelectContent position="popper">
+                                                    <SelectItem value="next">Next.js</SelectItem>
+                                                    <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                                                    <SelectItem value="astro">Astro</SelectItem>
+                                                    <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                                                </SelectContent>
+                                            </Select>
+                                        </div>
+                                    </div>
+                                </form>
+                            </CardContent>
+                            <CardFooter className="flex justify-end">
+                                <Button variant="outline">View Details</Button>
+                                
+                            </CardFooter>
+                        </Card>
 
-                  
                     </div>
 
 
 
                 </div>
+
             </div>
+
+
+
         </SidebarInset>
     );
 };
