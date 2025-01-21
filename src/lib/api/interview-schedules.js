c1d9c34b46995998bce3d42ce35c72d5dd385e14
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-21 15:27:25 +0530

    add interview schedules API functions

---
+++ src/lib/api/interview-schedules.js
@@ -0,0 +1,21 @@
+import axiosInstance from "./axioinstance";
+
+export const getAllInterviewSchedules =async(companyId) =>{
+
+    try {
+        const response=await axiosInstance.get(`/interview/schedules/${companyId}`);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const bookSchedule =async(data) =>{
+
+    try {
+        const response=await axiosInstance.post(`/interview/book-schedule/`, data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
