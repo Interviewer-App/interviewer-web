2c36619a706b5ef55354ce13e6ac0bd4e9c661d1
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-22 15:48:27 +0530

    feat: enhance slider component with dynamic background colors based on marks

--- src/components/ui/slider.jsx
+++ src/components/ui/slider.jsx
@@ -5,14 +5,14 @@ import * as SliderPrimitive from "@radix-ui/react-slider"
 
 import { cn } from "@/lib/utils"
 
-const Slider = React.forwardRef(({ className, ...props }, ref) => (
+const Slider = React.forwardRef(({ className, marks, ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
     className={cn("relative flex w-full touch-none select-none items-center", className)}
     {...props}>
     <SliderPrimitive.Track
-      className="relative h-2 w-full grow overflow-hidden rounded-full bg-lightred dark:bg-neutral-800">
-      <SliderPrimitive.Range className="absolute h-full bg-neutral-900 dark:bg-lightred" />
+      className={`relative h-2 w-full grow overflow-hidden rounded-full ${marks > 75 ? 'bg-green-600' : marks > 50? 'bg-yellow-500' : marks > 25 ? 'bg-orange-500' : 'bg-red-500' } dark:bg-neutral-800`}>
+      <SliderPrimitive.Range className={`absolute h-full ${marks > 75 ? 'bg-green-600' : marks > 50? 'bg-yellow-500' : marks > 25 ? 'bg-orange-500' : 'bg-red-500' }`} />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb
       className="block h-5 w-5 rounded-full border-2 border-neutral-900 bg-lightred ring-offset-lightred transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-50 dark:bg-neutral-950 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300" />
