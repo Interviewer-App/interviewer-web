39346a063743e2525ab05857fec674d5b93cd3c6
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-23 15:38:54 +0530

    fix: improve user fetching logic and add candidate detail retrieval

--- src/lib/api/users.js
+++ src/lib/api/users.js
@@ -1,28 +1,40 @@
-import axiosInstance from './axioinstance';
+import axiosInstance from "./axioinstance";
 
 const fetchUsers = async (page, limit, setLoading, setUsers, setTotalUsers) => {
-    setLoading(true);
-    try {
-        const response = await axiosInstance.get(`/users/${page}/${limit}`);
-        console.log(response); // Ensure you're logging the correct structure
+  setLoading(true);
+  try {
+    const response = await axiosInstance.get(`/users/${page}/${limit}`);
+    console.log(response); // Ensure you're logging the correct structure
 
-        if (!response || !response.data || !response.data.data) {
-            throw new Error(`Error fetching users: No data found`);
-        }
-
-        const data = response.data.data; // Access the user data here
-        const totalUsers = response.data.total; // Access the total users count
-        if (data && data.length > 0) {
-            setUsers(data); 
-            setTotalUsers(totalUsers); 
-        } else {
-            console.log("No users found.");
-        }
-    } catch (error) {
-        console.error("Error fetching users:", error);
-    } finally {
-        setLoading(false);
+    if (!response || !response.data || !response.data.data) {
+      throw new Error(`Error fetching users: No data found`);
     }
+
+    const data = response.data.data; // Access the user data here
+    const totalUsers = response.data.total; // Access the total users count
+    if (data && data.length > 0) {
+      setUsers(data);
+      setTotalUsers(totalUsers);
+    } else {
+      console.log("No users found.");
+    }
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  } finally {
+    setLoading(false);
+  }
+};
+
+export const getCandidateById = async (candidateId) => {
+  try {
+    const response = await axiosInstance.get(
+      `/users/candidate/details/${candidateId}`
+    );
+    return response;
+  } catch (error) {
+    console.log("Error fetching candidate details:", error);
+    throw error;
+  }
 };
 
 export { fetchUsers };
