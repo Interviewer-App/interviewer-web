81161ee07eb2ca16d90238c5c88a858626200387
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-27 15:39:32 +0530

    fix: adjust loading component positioning

--- src/app/loading.js
+++ src/app/loading.js
@@ -7,7 +7,7 @@ export default function Loading() {
   useEffect(() => {}, []);
 
   return (
-    <div className="flex flex-col h-lvh w-full bg-black/80 justify-center item-center">
+    <div className="flex fixed flex-col h-lvh w-full bg-black/80 z-[999] justify-center item-center">
       <div className=" w-full flex justify-center items-center">
         <PuffLoader color="#ffffff" />
       </div>
