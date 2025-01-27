ef42f475e3fbd45f212768ce1f481a34bac2b991
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-27 15:39:14 +0530

    feat: enhance loading state

--- src/components/company/invite-candidate-modal.js
+++ src/components/company/invite-candidate-modal.js
@@ -12,21 +12,27 @@ import {
 import { IoCloseCircle } from "react-icons/io5";
 import { Button } from "@/components/ui/button";
 import { getSession } from "next-auth/react";
-import { getInterviewTimeSlotsInterviewById, sendInvitaionForCandidates } from "@/lib/api/interview-invitation";
+import {
+  getInterviewTimeSlotsInterviewById,
+  sendInvitaionForCandidates,
+} from "@/lib/api/interview-invitation";
 import { useToast } from "@/hooks/use-toast";
-import { ToastAction } from "@/components/ui/toast"
+import { ToastAction } from "@/components/ui/toast";
+import Loading from "@/app/loading";
 
 function InviteCandidateModal({ setInviteModalOpen, interviewId }) {
   const [email, setEmail] = useState("");
   const [interviewTimeSlots, setInterviewTimeSlots] = useState({});
   const [interviewTimeSlotsDates, setInterviewTimeSlotsDates] = useState([]);
   const [filterInterviewTimeSlots, setFilterInterviewTimeSlots] = useState([]);
-  const [selectedTimeSlot, setSelectedTimeSlot] = useState('');
+  const [selectedTimeSlot, setSelectedTimeSlot] = useState("");
+  const [loading, setLoading] = useState(false);
   // const [interviewID, setInterviewID] = useState(interviewId);
   const { toast } = useToast();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
       const invitaionData = {
         to: email,
@@ -38,6 +44,7 @@ function InviteCandidateModal({ setInviteModalOpen, interviewId }) {
       if (response) {
         setInviteModalOpen(false);
       }
+      setLoading(false);
     } catch (err) {
       if (err.response) {
         const { data } = err.response;
@@ -80,33 +87,32 @@ function InviteCandidateModal({ setInviteModalOpen, interviewId }) {
           // Process dates and time slots
           const dates = Array.from(
             new Set(
-              scheduleData.map(group =>
-                new Date(group.date).toISOString().split('T')[0]
+              scheduleData.map(
+                (group) => new Date(group.date).toISOString().split("T")[0]
               )
             )
           ).sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
 
           const timeSlotsMap = scheduleData.reduce((acc, group) => {
-            const dateKey = new Date(group.date).toISOString().split('T')[0];
-            acc[dateKey] = group.schedules.map(slot => ({
+            const dateKey = new Date(group.date).toISOString().split("T")[0];
+            acc[dateKey] = group.schedules.map((slot) => ({
               scheduleID: slot.id,
               startTime: new Date(slot.start).toLocaleTimeString([], {
-                hour: '2-digit',
-                minute: '2-digit',
-                hour12: true
+                hour: "2-digit",
+                minute: "2-digit",
+                hour12: true,
               }),
               endTime: new Date(slot.end).toLocaleTimeString([], {
-                hour: '2-digit',
-                minute: '2-digit',
-                hour12: true
+                hour: "2-digit",
+                minute: "2-digit",
+                hour12: true,
               }),
-              isBooked: slot.isBooked
+              isBooked: slot.isBooked,
             }));
             return acc;
           }, {});
 
-
-          setInterviewTimeSlotsDates(dates)
+          setInterviewTimeSlotsDates(dates);
           setInterviewTimeSlots(timeSlotsMap);
           setFilterInterviewTimeSlots(timeSlotsMap);
         }
@@ -122,98 +128,107 @@ function InviteCandidateModal({ setInviteModalOpen, interviewId }) {
     fetchInterviewTimeSlots();
   }, []);
 
-
-
   return (
-    <div className=" fixed  top-0 left-0 z-50 h-full w-full flex items-center justify-center bg-black/50">
-      <div className=" relative max-w-[700px] h-fit max-h-[670px] w-[90%] md:w-[50%] p-9 bg-gradient-to-br from-[#1f2126] to-[#17191d] rounded-lg">
-        <h1 className=" text-2xl font-semibold text-[#f3f3f3] pb-5">
-          Invite Candidate
-        </h1>
-        <button
-          type="button"
-          onClick={() => setInviteModalOpen(false)}
-          className=" absolute top-5 right-5 text-[#f3f3f3]"
-        >
-          <MdClose className=" text-2xl" />
-        </button>
-        <p className=" text-sm text-gray-500 py-2">
-          Enter the candidate's email address and click the "Send Invitation" button. Once
-          submitted, an interview session link will be sent directly to the
-          candidate's email.
-        </p>
-        <form onSubmit={handleSubmit} >
-          <div className=" flex w-full justify-between space-x-2 mt-5">
-          <div className="flex-1">
-            <input
-              type="email"
-              placeholder="Canadidate's Email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="h-[45px] w-full rounded-lg text-sm border-0 bg-[#32353b] placeholder-[#737883] px-6 py-2 mb-5"
-            />
-          </div>
-          <div className="flex-1">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  className="!bg-[#32353b] w-full h-[45px] m-0 px-2 focus:outline-none outline-none"
-                  variant="outline"
-                >
-                  {Object.values(interviewTimeSlots)
-                    .flat()
-                    .find(slot => slot.scheduleID === selectedTimeSlot)?.startTime || "Select Time Slot"}
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56">
-                <DropdownMenuLabel>Available Time Slots</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuRadioGroup
-                  value={selectedTimeSlot}
-                  onValueChange={setSelectedTimeSlot}
-                >
-                  {interviewTimeSlotsDates?.map(date => (
-                    <div key={date}>
-                      <DropdownMenuLabel className="text-xs text-gray-400">
-                        {new Date(date).toLocaleDateString(undefined, {
-                          weekday: 'short',
-                          year: 'numeric',
-                          month: 'short',
-                          day: 'numeric'
-                        })}
-                      </DropdownMenuLabel>
-                      {interviewTimeSlots[date]?.map(slot => (
-                        <DropdownMenuRadioItem
-                          key={slot.scheduleID}
-                          value={slot.scheduleID}
-                          disabled={slot.isBooked}
-                        >
-                          <div className="flex justify-between items-center w-full">
-                            <span>{slot.startTime} - {slot.endTime}</span>
-                            {slot.isBooked && (
-                              <span className="text-red-500 text-xs ml-2">Booked</span>
-                            )}
-                          </div>
-                        </DropdownMenuRadioItem>
+    <>
+      <div className=" fixed  top-0 left-0 z-50 h-full w-full flex items-center justify-center bg-black/50">
+        <div className=" relative max-w-[700px] h-fit max-h-[670px] w-[90%] md:w-[50%] p-9 bg-gradient-to-br from-[#1f2126] to-[#17191d] rounded-lg">
+          <h1 className=" text-2xl font-semibold text-[#f3f3f3] pb-5">
+            Invite Candidate
+          </h1>
+          <button
+            type="button"
+            onClick={() => setInviteModalOpen(false)}
+            className=" absolute top-5 right-5 text-[#f3f3f3]"
+          >
+            <MdClose className=" text-2xl" />
+          </button>
+          <p className=" text-sm text-gray-500 py-2">
+            Enter the candidate's email address and click the "Send Invitation"
+            button. Once submitted, an interview session link will be sent
+            directly to the candidate's email.
+          </p>
+          <form onSubmit={handleSubmit}>
+            <div className=" flex w-full justify-between space-x-2 mt-5">
+              <div className="flex-1">
+                <input
+                  type="email"
+                  placeholder="Canadidate's Email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className="h-[45px] w-full rounded-lg text-sm border-0 bg-[#32353b] placeholder-[#737883] px-6 py-2 mb-5"
+                />
+              </div>
+              <div className="flex-1">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button
+                      className="!bg-[#32353b] w-full h-[45px] m-0 px-2 focus:outline-none outline-none"
+                      variant="outline"
+                    >
+                      {Object.values(interviewTimeSlots)
+                        .flat()
+                        .find((slot) => slot.scheduleID === selectedTimeSlot)
+                        ?.startTime || "Select Time Slot"}
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-56">
+                    <DropdownMenuLabel>Available Time Slots</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuRadioGroup
+                      value={selectedTimeSlot}
+                      onValueChange={setSelectedTimeSlot}
+                    >
+                      {interviewTimeSlotsDates?.map((date) => (
+                        <div key={date}>
+                          <DropdownMenuLabel className="text-xs text-gray-400">
+                            {new Date(date).toLocaleDateString(undefined, {
+                              weekday: "short",
+                              year: "numeric",
+                              month: "short",
+                              day: "numeric",
+                            })}
+                          </DropdownMenuLabel>
+                          {interviewTimeSlots[date]?.map((slot) => (
+                            <DropdownMenuRadioItem
+                              key={slot.scheduleID}
+                              value={slot.scheduleID}
+                              disabled={slot.isBooked}
+                            >
+                              <div className="flex justify-between items-center w-full">
+                                <span>
+                                  {slot.startTime} - {slot.endTime}
+                                </span>
+                                {slot.isBooked && (
+                                  <span className="text-red-500 text-xs ml-2">
+                                    Booked
+                                  </span>
+                                )}
+                              </div>
+                            </DropdownMenuRadioItem>
+                          ))}
+                        </div>
                       ))}
-                    </div>
-                  ))}
-                </DropdownMenuRadioGroup>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-          </div>
-          <div className=" flex w-full justify-center mt-4">
-            <button type="submit" className=" py-2.5 bg-white text-black text-sm font-semibold rounded-lg px-5 w-full">
-              Send Invitation
-            </button>
-          </div>
-        </form>
-        
+                    </DropdownMenuRadioGroup>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            </div>
+            <div className=" flex w-full justify-center mt-4">
+              <button
+                type="submit"
+                disabled={loading}
+                className=" py-2.5 bg-white text-black text-sm font-semibold rounded-lg px-5 w-full"
+              >
+                Send Invitation
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+      {loading && <Loading />}
+    </>
   );
 }
 
