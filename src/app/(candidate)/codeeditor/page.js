416e4da3b61420cf0b854e7272d06bf4e1b194f5
Madhawa Awishka <madhawaawishka@gmail.com>
2025-01-27 17:01:47 +0530

    make codeeditor compnent for intialization testing

---
+++ src/app/(candidate)/codeeditor/page.js
@@ -0,0 +1,14 @@
+import CodeEditor from '@/components/CodeEditor/CodeEditor'
+import React from 'react'
+
+function Codeeditor() {
+  return (
+
+    <div className='w-full'>
+        <CodeEditor/>
+    </div>
+
+  )
+}
+
+export default Codeeditor
