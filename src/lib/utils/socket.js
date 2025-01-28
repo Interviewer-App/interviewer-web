4c3ed4332397c435b6b61df17d2d3e6210017c38
ushansankalpa <ushansan99@gmail.com>
2025-01-28 14:31:06 +0530

    fixed issues

--- src/lib/utils/socket.js
+++ src/lib/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 
-const socket = io('http://localhost:3333'); // Adjust the URL to your backend
+const socket = io(process.env.NEXT_PUBLIC_API_URL_SOCKET); // Adjust the URL to your backend
 
 export default socket;
