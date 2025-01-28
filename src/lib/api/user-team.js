04d350bf231f6cb8938f1cb4ea6779bad066bd76
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-28 17:22:17 +0530

    refactor getCompanyTeams function for improved error handling and return response

--- src/lib/api/user-team.js
+++ src/lib/api/user-team.js
@@ -1,34 +1,26 @@
 import axiosInstance from "./axioinstance";
 
-export const getCompanyTeams = async (page, limit, companyId, setTeams, setTotalTeams) => {
-    try {
-      const response = await axiosInstance.get(`/users/company/team/${page}/${limit}/${companyId}`);
-  
-      if (!response || !response.data || !response.data.data) {
-        throw new Error(`Error fetching teams: No data found`);
-      }
-  
-      const data = response.data.team; 
-      const totalTeams = response.data.total;
-      if (data && data.length > 0) {
-        setTeams(data);
-        setTotalTeams(totalTeams);
-      } else {
-        console.log("No teams found.");
-      }
-    } catch (error) {
-      console.log("Error fetching teams:", error);
-    } 
-  };
+export const getCompanyTeams = async (companyId, page, limit) => {
+  try {
+    const response = await axiosInstance.get(
+      `/users/company/team/${page}/${limit}/${companyId}`
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
-  export const createTeam = async (data) => {
-    try {
-      const response = await axiosInstance.post(
-        `/users/register/team/member/`, data
-      );
-      return response;
-    } catch (error) {
-      console.log("Error creating team:", error);
-      throw error;
-    }
-  };
+export const createTeam = async (data) => {
+  try {
+    const response = await axiosInstance.post(
+      `/users/register/team/member/`,
+      data
+    );
+    return response;
+  } catch (error) {
+    console.log("Error creating team:", error);
+    throw error;
+  }
+};
