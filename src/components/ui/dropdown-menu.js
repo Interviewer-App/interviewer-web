3b80d0d2ee660b8a4a5e1dcf05614d0b846f4e1b
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-28 17:25:49 +0530

    fix: standardize semicolons and formatting

--- src/components/ui/dropdown-menu.js
+++ src/components/ui/dropdown-menu.js
@@ -1,22 +1,22 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu"
-import { Check, ChevronRight, Circle } from "lucide-react"
+import * as React from "react";
+import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
+import { Check, ChevronRight, Circle } from "lucide-react";
 
-import { cn } from "@/lib/utils/utill"
+import { cn } from "@/lib/utils/utill";
 
-const DropdownMenu = DropdownMenuPrimitive.Root
+const DropdownMenu = DropdownMenuPrimitive.Root;
 
-const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger
+const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
 
-const DropdownMenuGroup = DropdownMenuPrimitive.Group
+const DropdownMenuGroup = DropdownMenuPrimitive.Group;
 
-const DropdownMenuPortal = DropdownMenuPrimitive.Portal
+const DropdownMenuPortal = DropdownMenuPrimitive.Portal;
 
-const DropdownMenuSub = DropdownMenuPrimitive.Sub
+const DropdownMenuSub = DropdownMenuPrimitive.Sub;
 
-const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup
+const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup;
 
 const DropdownMenuSubTrigger = React.forwardRef(
   ({ className, inset, children, ...props }, ref) => (
@@ -33,9 +33,9 @@ const DropdownMenuSubTrigger = React.forwardRef(
       <ChevronRight className="ml-auto h-4 w-4" />
     </DropdownMenuPrimitive.SubTrigger>
   )
-)
+);
 DropdownMenuSubTrigger.displayName =
-  DropdownMenuPrimitive.SubTrigger.displayName
+  DropdownMenuPrimitive.SubTrigger.displayName;
 
 const DropdownMenuSubContent = React.forwardRef(
   ({ className, ...props }, ref) => (
@@ -48,9 +48,9 @@ const DropdownMenuSubContent = React.forwardRef(
       {...props}
     />
   )
-)
+);
 DropdownMenuSubContent.displayName =
-  DropdownMenuPrimitive.SubContent.displayName
+  DropdownMenuPrimitive.SubContent.displayName;
 
 const DropdownMenuContent = React.forwardRef(
   ({ className, sideOffset = 4, ...props }, ref) => (
@@ -59,15 +59,15 @@ const DropdownMenuContent = React.forwardRef(
         ref={ref}
         sideOffset={sideOffset}
         className={cn(
-          "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+          "z-50 min-w-[8rem] !bg-black overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
           className
         )}
         {...props}
       />
     </DropdownMenuPrimitive.Portal>
   )
-)
-DropdownMenuContent.displayName = DropdownMenuPrimitive.Content.displayName
+);
+DropdownMenuContent.displayName = DropdownMenuPrimitive.Content.displayName;
 
 const DropdownMenuItem = React.forwardRef(
   ({ className, inset, ...props }, ref) => (
@@ -81,8 +81,8 @@ const DropdownMenuItem = React.forwardRef(
       {...props}
     />
   )
-)
-DropdownMenuItem.displayName = DropdownMenuPrimitive.Item.displayName
+);
+DropdownMenuItem.displayName = DropdownMenuPrimitive.Item.displayName;
 
 const DropdownMenuCheckboxItem = React.forwardRef(
   ({ className, children, checked, ...props }, ref) => (
@@ -103,9 +103,9 @@ const DropdownMenuCheckboxItem = React.forwardRef(
       {children}
     </DropdownMenuPrimitive.CheckboxItem>
   )
-)
+);
 DropdownMenuCheckboxItem.displayName =
-  DropdownMenuPrimitive.CheckboxItem.displayName
+  DropdownMenuPrimitive.CheckboxItem.displayName;
 
 const DropdownMenuRadioItem = React.forwardRef(
   ({ className, children, ...props }, ref) => (
@@ -125,8 +125,8 @@ const DropdownMenuRadioItem = React.forwardRef(
       {children}
     </DropdownMenuPrimitive.RadioItem>
   )
-)
-DropdownMenuRadioItem.displayName = DropdownMenuPrimitive.RadioItem.displayName
+);
+DropdownMenuRadioItem.displayName = DropdownMenuPrimitive.RadioItem.displayName;
 
 const DropdownMenuLabel = React.forwardRef(
   ({ className, inset, ...props }, ref) => (
@@ -140,8 +140,8 @@ const DropdownMenuLabel = React.forwardRef(
       {...props}
     />
   )
-)
-DropdownMenuLabel.displayName = DropdownMenuPrimitive.Label.displayName
+);
+DropdownMenuLabel.displayName = DropdownMenuPrimitive.Label.displayName;
 
 const DropdownMenuSeparator = React.forwardRef(
   ({ className, ...props }, ref) => (
@@ -151,8 +151,8 @@ const DropdownMenuSeparator = React.forwardRef(
       {...props}
     />
   )
-)
-DropdownMenuSeparator.displayName = DropdownMenuPrimitive.Separator.displayName
+);
+DropdownMenuSeparator.displayName = DropdownMenuPrimitive.Separator.displayName;
 
 const DropdownMenuShortcut = ({ className, ...props }) => {
   return (
@@ -160,9 +160,9 @@ const DropdownMenuShortcut = ({ className, ...props }) => {
       className={cn("ml-auto text-xs tracking-widest opacity-60", className)}
       {...props}
     />
-  )
-}
-DropdownMenuShortcut.displayName = "DropdownMenuShortcut"
+  );
+};
+DropdownMenuShortcut.displayName = "DropdownMenuShortcut";
 
 export {
   DropdownMenu,
@@ -179,5 +179,5 @@ export {
   DropdownMenuSub,
   DropdownMenuSubContent,
   DropdownMenuSubTrigger,
-  DropdownMenuRadioGroup
-}
+  DropdownMenuRadioGroup,
+};
