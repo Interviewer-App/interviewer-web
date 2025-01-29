adcc235992f59df01dcb554629279286a191eb6d
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-29 11:41:38 +0530

    refactor: improve loading component structure and styling

--- src/app/loading.js
+++ src/app/loading.js
@@ -7,13 +7,15 @@ export default function Loading() {
   useEffect(() => {}, []);
 
   return (
-    <div className="flex fixed flex-col h-lvh w-full bg-black/80 z-[999] justify-center item-center">
-      <div className=" w-full flex justify-center items-center">
-        <PuffLoader color="#ffffff" />
+    <div className="fixed top-0 left-0 w-full h-lvh bg-black/80 z-[999]">
+      <div className="flex flex-col h-lvh w-full bg-black/80 justify-center item-center">
+        <div className=" w-full flex justify-center items-center">
+          <PuffLoader color="#ffffff" />
+        </div>
+        <p className=" w-full text-center font-semibold text-xl py-5">
+          Loading...
+        </p>
       </div>
-      <p className=" w-full text-center font-semibold text-xl py-5">
-        Loading...
-      </p>
     </div>
   );
 }
