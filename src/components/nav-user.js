8ee89181ce1fc751bfdb6a612dfa8a22c07edff6
Madhawa Awishka <madhawaawishka@gmail.com>
2025-01-30 12:49:50 +0530

    adding cursor pointer to the Log out button

--- src/components/nav-user.js
+++ src/components/nav-user.js
@@ -101,7 +101,7 @@ export function NavUser({ user }) {
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator /> */}
-            <DropdownMenuItem onClick={signout}>
+            <DropdownMenuItem onClick={signout} className='cursor-pointer'>
               <LogOut />
               Log out
             </DropdownMenuItem>
