1fc03905aaa2522e8577a6a0e249005e5dd98ee6
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-01-31 13:40:18 +0530

    feat: add PDF viewer component

---
+++ src/components/candidate/pdf-viewer.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+// Set up the worker for PDF.js
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+function PDFViewer({ pdfUrl }) {
+  const [numPages, setNumPages] = useState(null);
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+  return (
+    <div>
+      <Document
+        file={pdfUrl}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+        ))}
+      </Document>
+    </div>
+  );
+}
+
+export default PDFViewer;
