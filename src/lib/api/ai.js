485369c4ff866665847a9a4974c2cd2551e7d1f6
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-02-03 15:14:43 +0530

    Add generateInterviewQuestions function to handle question generation for specific interviews

--- src/lib/api/ai.js
+++ src/lib/api/ai.js
@@ -11,6 +11,17 @@ export const generateQuestions = async (sessionId, data) => {
   }
 };
 
+export const generateInterviewQuestions = async (interviewId, data) => {
+  try {
+    const response = await axiosInstance.post(`/ai/generate-questions-interview/${interviewId}`, data
+    );
+    return response;
+  } catch (error) {
+    console.log("Error generating questions:", error);
+    throw error;
+  }
+};
+
 export const analiyzeQuestion = async (data) => {
   try {
     const response = await axiosInstance.post(`/ai/analiyze-question`, data);
