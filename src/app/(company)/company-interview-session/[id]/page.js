06f6adf562eb71001a8bcbe6fda61149beb57877
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-02-05 14:47:02 +0530

    implement drag-and-drop functionality for question arrangement using DnD Kit

--- src/app/(company)/company-interview-session/[id]/page.js
+++ src/app/(company)/company-interview-session/[id]/page.js
@@ -1,7 +1,6 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import { useToast } from "@/hooks/use-toast";
-import socket from "../../../../lib/utils/socket";
 import { ToastAction } from "@/components/ui/toast";
 import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 import { Separator } from "@radix-ui/react-dropdown-menu";
@@ -14,6 +13,32 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { getSession } from "next-auth/react";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+
+import {
+  restrictToVerticalAxis,
+  restrictToParentElement,
+} from "@dnd-kit/modifiers";
+import SortableLinks from "@/components/SortableLinks";
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card";
+import { FaDotCircle } from "react-icons/fa";
 
 //API
 import { getInterviewSessionById } from "@/lib/api/interview-session";
@@ -35,9 +60,22 @@ function InterviewSessionPreviewPage({ params }) {
   const [isQuestionEdit, setIsQuestionEdit] = useState(false);
   const [createModalOpen, setCreateModalOpen] = useState(false);
   const [generateModalOpen, setGenerateModalOpen] = useState(false);
+  const [tab, setTab] = useState("technical");
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const [items, setItems] = useState([
+    { name: "NextJS", id: 1693653637084 },
+    { name: "ReactJS", id: 1693653637086 },
+    { name: "Astro", id: 1693653637088 },
+    { name: "Vue", id: 1693653637090 },
+  ]);
 
   const { toast } = useToast();
-  // const { socket } = useSocket();
   const router = useRouter();
 
   useEffect(() => {
@@ -75,6 +113,15 @@ function InterviewSessionPreviewPage({ params }) {
   useEffect(() => {
     console.log("interviewId", interviewId);
   }, [interviewId]);
+
+  useEffect(() => {
+    if (!sessionDetails || !sessionDetails.CategoryScore) return;
+    const tabData = sessionDetails.CategoryScore.map((category) => ({
+      name: category.categoryAssignment.category.categoryName,
+      id: category.categoryAssignment.category.categoryId,
+    }));
+    setItems(tabData);
+  }, [sessionDetails]);
 
   const interviewStart = async (e) => {
     const session = await getSession();
@@ -127,6 +174,19 @@ function InterviewSessionPreviewPage({ params }) {
       });
     }
   };
+
+  function handleDragEnd(event) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((prevItems) => {
+        const oldIndex = prevItems.findIndex((item) => item.id === active.id);
+        const newIndex = prevItems.findIndex((item) => item.id === over.id);
+
+        return arrayMove(prevItems, oldIndex, newIndex);
+      });
+    }
+  }
 
   return (
     <>
@@ -200,8 +260,7 @@ function InterviewSessionPreviewPage({ params }) {
                     ""}
                 </p>
                 <p className=" text-base pt-1 text-gray-400">
-                  Type:{" "}
-                  {sessionDetails?.interviewCategory || ""} Interview
+                  Type: {sessionDetails?.interviewCategory || ""} Interview
                 </p>
               </div>
               <div className=" w-full md:w-[50%] flex items-center justify-start md:justify-end mt-5 md:mt-0">
@@ -216,48 +275,141 @@ function InterviewSessionPreviewPage({ params }) {
             </div>
 
             <div className="mt-5 bg-slate-500/10 p-9 rounded-lg">
-              <div className=" w-full  flex flex-col md:flex-row items-center justify-between">
-                <h1 className=" text-2xl font-semibold text-left w-full">
-                  Questions
-                </h1>
-                <div className=" w-full flex items-center justify-end">
+              <div className=" w-full flex justify-between items-center">
+                <div className="flex space-x-4 bg-slate-600/20 w-fit p-1 md:p-2 rounded-lg">
                   <button
-                    className=" h-11 min-w-[160px] mt-5 md:mt-0 px-5 mr-5 cursor-pointer bg-white rounded-lg text-center text-black font-semibold"
+                    onClick={() => setTab("technical")}
+                    className={` text-xs md:text-sm py-2 px-4 md:px-6 rounded-lg ${
+                      tab === "technical" ? "bg-gray-800" : ""
+                    } `}
+                  >
+                    Technical
+                  </button>
+                  <button
+                    onClick={() => setTab("others")}
+                    className={` text-xs md:text-sm py-2 px-4 md:px-6 rounded-lg ${
+                      tab === "others" ? "bg-gray-800" : ""
+                    } `}
+                  >
+                    Others
+                  </button>
+                </div>
+                <div
+                  className={`${
+                    tab === "technical" ? "block" : "hidden"
+                  } w-full p-1 md:p-2 flex items-center justify-end`}
+                >
+                  <button
+                    className=" h-11 min-w-[160px] md:mt-0 px-5 mr-5 cursor-pointer bg-white rounded-lg text-center text-black font-semibold"
                     onClick={() => setGenerateModalOpen(true)}
                   >
                     Genarate questions
                   </button>
                   <button
                     onClick={() => setCreateModalOpen(true)}
-                    className=" h-11 min-w-[160px] mt-5 md:mt-0 cursor-pointer bg-white text-black rounded-lg text-center font-semibold"
+                    className=" h-11 min-w-[160px] md:mt-0 cursor-pointer bg-white text-black rounded-lg text-center font-semibold"
                   >
                     {" "}
                     + Add Question
                   </button>
                 </div>
               </div>
-              {sessionDetails.questions?.length > 0 ? (
-                sessionDetails.questions.map((question, index) => (
-                  <QuestionDisplayCard
-                    forSession={true}
-                    key={index}
-                    index={index}
-                    question={question}
-                    isQuestionEdit={isQuestionEdit}
-                    setIsQuestionEdit={setIsQuestionEdit}
-                  />
-                ))
-              ) : (
-                <div className=" w-full flex flex-col items-center justify-center min-h-[300px] gap-2">
-                  <p>No questions available.</p>
-                  <button
-                    className=" h-11 min-w-[160px] md:mt-0 px-5 mt-5 cursor-pointer border-2 hover:bg-white/30 border-white rounded-lg text-center text-white font-semibold"
-                    onClick={handleImportQuestions}
-                  >
-                    Import questions
-                  </button>
+
+              <div className=" flex justify-between items-start">
+                {tab === "technical" ? (
+                  <div className=" w-[68%] mt-3 border-r-2 border-gray-700/20 pr-9">
+                    <div className=" w-full flex flex-col md:flex-row items-center justify-between">
+                      <h1 className=" text-2xl font-semibold text-left w-full">
+                        Questions
+                      </h1>
+                    </div>
+                    {sessionDetails.questions?.length > 0 ? (
+                      sessionDetails.questions.map((question, index) => (
+                        <QuestionDisplayCard
+                          forSession={true}
+                          key={index}
+                          index={index}
+                          question={question}
+                          isQuestionEdit={isQuestionEdit}
+                          setIsQuestionEdit={setIsQuestionEdit}
+                        />
+                      ))
+                    ) : (
+                      <div className=" w-full flex flex-col items-center justify-center min-h-[300px] gap-2">
+                        <p>No questions available.</p>
+                        <button
+                          className=" h-11 min-w-[160px] md:mt-0 px-5 mt-5 cursor-pointer border-2 hover:bg-white/30 border-white rounded-lg text-center text-white font-semibold"
+                          onClick={handleImportQuestions}
+                        >
+                          Import questions
+                        </button>
+                      </div>
+                    )}
+                  </div>
+                ) : (
+                  <div className=" w-[68%] mt-3 border-r-2 border-gray-700/20">
+                    {sessionDetails.CategoryScore.filter(
+                      (category) =>
+                        category.categoryAssignment.category.categoryName !==
+                        "Technical"
+                    ).map((category) => (
+                      <div
+                        key={category.categoryScoreId}
+                        className=" w-full mt-5 "
+                      >
+                        <h1 className=" text-2xl font-semibold text-left w-full">
+                          <FaDotCircle className=" text-blue-700 text-xs inline-block mr-2" />
+                          {category.categoryAssignment.category.categoryName} :{" "}
+                          {category.categoryAssignment.percentage}%{" "}
+                        </h1>
+
+                        <div className=" px-8">
+                          {category.categoryAssignment.SubCategoryAssignment.map(
+                            (subCategory) => (
+                              <div
+                                key={subCategory.id}
+                                className=" mt-3 px-5 py-2 rounded-lg border-2 border-gray-600 text-gray-400 bg-gray-500/20"
+                              >
+                                <h1 className=" font-semibold">
+                                  {subCategory.name}
+                                </h1>
+                                <p>{subCategory.percentage}%</p>
+                              </div>
+                            )
+                          )}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                )}
+                <div className=" w-[30%] mt-3 px-5">
+                  <h1 className=" text-2xl font-semibold text-left w-full">
+                    Question arrangement
+                  </h1>
+                  <Card className="w-full !border-0 !bg-transparent md:max-w-lg">
+                    <CardContent className="grid gap-4 p-5 bg-gray-700/20 mt-5 text-gray-400 border-2 border-gray-700 rounded-lg">
+                      <DndContext
+                        sensors={sensors}
+                        collisionDetection={closestCenter}
+                        onDragEnd={handleDragEnd}
+                        modifiers={[
+                          restrictToVerticalAxis,
+                          restrictToParentElement,
+                        ]}
+                      >
+                        <SortableContext
+                          items={items}
+                          strategy={verticalListSortingStrategy}
+                        >
+                          {items.map((item) => (
+                            <SortableLinks key={item.id} id={item} />
+                          ))}
+                        </SortableContext>
+                      </DndContext>
+                    </CardContent>
+                  </Card>
                 </div>
-              )}
+              </div>
             </div>
           </div>
           {createModalOpen && (
