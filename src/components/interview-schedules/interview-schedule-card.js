c4508c34721a3af91b2c92fe4a257b2069a604be
Madhawa Awishka <madhawaawishka@gmail.com>
2025-02-06 11:05:55 +0530

    Add ToolTip to Interview schedule cards

--- src/components/interview-schedules/interview-schedule-card.js
+++ src/components/interview-schedules/interview-schedule-card.js
@@ -3,7 +3,7 @@ import { getSession } from "next-auth/react";
 import React, { useEffect } from "react";
 import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "@/components/ui/toast";
-import  socket  from '../../lib/utils/socket';
+import socket from '../../lib/utils/socket';
 import {
   Tooltip,
   TooltipContent,
@@ -14,98 +14,110 @@ import {
 import { useRouter } from "next/navigation";
 
 
-export default function InterviewScheduleCard({ index, interview,showButton=true}) {
-    const { toast } = useToast();
+export default function InterviewScheduleCard({ index, interview, showButton = true }) {
+  const { toast } = useToast();
 
-   const router = useRouter();
+  const router = useRouter();
 
-    const handleClick = () => {
-      router.push(`/interview-schedules/${encodeURIComponent(interview.interviewID)}`);
-    };
+  const handleClick = () => {
+    router.push(`/interview-schedules/${encodeURIComponent(interview.interviewID)}`);
+  };
 
-    const joinInterviewSession = async (e) => {
-      e.preventDefault();
-      try {
-        const session = await getSession();
-        const candidateID = session?.user?.candidateID;
-        const interviewSessionData = {
-          candidateId: candidateID,
-          interviewId: interview.interviewID,
-          interviewCategory: interview.interviewCategory,
-          scheduledDate: interview.scheduledDate,
-          scheduledAt: interview.scheduledAt,
-          interviewStatus: "toBeConducted",
-        };
+  const joinInterviewSession = async (e) => {
+    e.preventDefault();
+    try {
+      const session = await getSession();
+      const candidateID = session?.user?.candidateID;
+      const interviewSessionData = {
+        candidateId: candidateID,
+        interviewId: interview.interviewID,
+        interviewCategory: interview.interviewCategory,
+        scheduledDate: interview.scheduledDate,
+        scheduledAt: interview.scheduledAt,
+        interviewStatus: "toBeConducted",
+      };
 
-        const response = await createInterviewSession(interviewSessionData);
-  
-        if (response) {
-          const sessionId = response.data.interviewSession.sessionId;
-          const role = session?.user?.role;
-          const userId = session?.user?.id;
-          const data = {
-            sessionId: sessionId,
-            userId: userId,
-            role: role
-          }
-          socket.emit('joinInterviewSession', data);
-          router.push(`/interview-room/${sessionId}`);
+      const response = await createInterviewSession(interviewSessionData);
+
+      if (response) {
+        const sessionId = response.data.interviewSession.sessionId;
+        const role = session?.user?.role;
+        const userId = session?.user?.id;
+        const data = {
+          sessionId: sessionId,
+          userId: userId,
+          role: role
         }
-      } catch (err) {
-        if (err.response) {
-          const { data } = err.response;
-  
-          if (data && data.message) {
-            toast({
-              variant: "destructive",
-              title: "Uh oh! Something went wrong.",
-              description: `Interview create failed: ${data.message}`,
-              action: <ToastAction altText="Try again">Try again</ToastAction>,
-            });
-          } else {
-            toast({
-              variant: "destructive",
-              title: "Uh oh! Something went wrong.",
-              description: "An unexpected error occurred. Please try again.",
-              action: <ToastAction altText="Try again">Try again</ToastAction>,
-            });
-          }
+        socket.emit('joinInterviewSession', data);
+        router.push(`/interview-room/${sessionId}`);
+      }
+    } catch (err) {
+      if (err.response) {
+        const { data } = err.response;
+
+        if (data && data.message) {
+          toast({
+            variant: "destructive",
+            title: "Uh oh! Something went wrong.",
+            description: `Interview create failed: ${data.message}`,
+            action: <ToastAction altText="Try again">Try again</ToastAction>,
+          });
         } else {
           toast({
             variant: "destructive",
             title: "Uh oh! Something went wrong.",
-            description:
-              "An unexpected error occurred. Please check your network and try again.",
+            description: "An unexpected error occurred. Please try again.",
             action: <ToastAction altText="Try again">Try again</ToastAction>,
           });
         }
+      } else {
+        toast({
+          variant: "destructive",
+          title: "Uh oh! Something went wrong.",
+          description:
+            "An unexpected error occurred. Please check your network and try again.",
+          action: <ToastAction altText="Try again">Try again</ToastAction>,
+        });
       }
-    };
+    }
+  };
 
   return (
-    <div className=" relative w-full h-full flex flex-col items-center justify-center rounded-xl p-6 bg-gray-800/80 hover:bg-gray-800/90 transition-all duration-300"
-    onClick={handleClick} >
-      <TooltipProvider>
-        <Tooltip>
-          <TooltipTrigger>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <div className=" relative w-full h-full flex flex-col items-center justify-center rounded-xl p-6 bg-gray-800/80 hover:bg-gray-800/90 transition-all duration-300 cursor-pointer"
+            onClick={handleClick} >
             <p className="text-3xl font-semibold pb-6 text-center text-ellipsis overflow-hidden whitespace-nowrap max-w-[300px]">
               {interview.jobTitle}
             </p>
-          </TooltipTrigger>
-          <TooltipContent>{interview.jobTitle || ""}</TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-      <h3 className=" text-sm md:text-base">Start Date: {new Date(interview.startDate).toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' })}</h3>
-      <h3 className=" text-sm md:text-base">end Date: {new Date(interview.endDate).toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' })}</h3>
-      {showButton && (
-      <button
-        type="button"
-        className=" h-12 min-w-[150px] w-full md:w-[40%] mt-8 cursor-pointer bg-white rounded-lg text-center text-base text-black font-semibold"
-        onClick={joinInterviewSession}
-      >
-        Join Now
-      </button>
-      )}
-    </div>
+            <h3 className=" text-sm md:text-base">Start Date: {new Date(interview.startDate).toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' })}</h3>
+            <h3 className=" text-sm md:text-base">end Date: {new Date(interview.endDate).toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' })}</h3>
+            {
+              showButton && (
+                <button
+                  type="button"
+                  className=" h-12 min-w-[150px] w-full md:w-[40%] mt-8 cursor-pointer bg-white rounded-lg text-center text-base text-black font-semibold"
+                  onClick={joinInterviewSession}
+                >
+                  Join Now
+                </button>
+              )
+            }
+          </div >
+        </TooltipTrigger>
+        <TooltipContent className="max-w-[400px] bg-gray-800 text-white border-gray-700">
+          <div className="p-2">
+            <h3 className="font-bold text-lg mb-2">Comapany:{interview.companyName}</h3>
+            <h3 className="font-bold text-lg mb-2">Interview Category:{interview.interviewCategory}</h3>
+            <div
+              className="text-sm text-gray-300"
+              dangerouslySetInnerHTML={{ __html: interview.jobDescription }}
+            />
+          </div>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+
   );
 }
