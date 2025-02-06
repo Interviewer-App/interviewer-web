1dc9adb0539b8809506972ab2d1a8d68e25c4222
Madhawa Awishka <madhawaawishka@gmail.com>
2025-02-06 16:54:00 +0530

    set tooltip to interviewDisplayCard

--- src/components/interviews/interview-display-card.js
+++ src/components/interviews/interview-display-card.js
@@ -6,25 +6,46 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-
-export default function InterviewDisplayCard({ index, interview }) {
+import { useEffect, useState } from "react";
+import { getInterviewOverviewById } from "@/lib/api/interview-session";
+export default function InterviewDisplayCard({ index, interview,hoveredId }) {
   const router = useRouter();
+  const [interviewOverview, setInterviewOverview] = useState({});
+
 
   const navigationClickHandler = () => {
     router.push(`/interviews/${encodeURIComponent(interview.interviewID)}`);
   };
-  
+
+  useEffect(() => {
+    const fetchOverviewData = async () => {
+      try {
+        const response = await getInterviewOverviewById(interview.interviewID);
+        // console.log(response.data);
+        if (response.data) {
+          setInterviewOverview(response.data);
+        }
+      } catch (error) {
+        console.log("Error fetching interview overview:", error);
+      }
+    };
+
+    if (interview.interviewID == hoveredId) {
+      fetchOverviewData();
+    }
+  }, [hoveredId]);
+
 
   return (
     <TooltipProvider>
       <Tooltip>
-        <TooltipTrigger>
+        <TooltipTrigger className="!w-full">
           <div
             onClick={navigationClickHandler}
             className=" group relative w-full h-full flex flex-col items-center justify-center rounded-xl py-8 px-6 bg-gray-800/80 cursor-pointer hover:bg-gray-800/90 transition-all duration-300"
           >
 
-            <p className="text-3xl font-semibold pb-6 text-center text-ellipsis overflow-hidden whitespace-nowrap max-w-[300px]">
+            <p className="text-lg font-semibold pb-6 text-center text-ellipsis overflow-hidden whitespace-nowrap lg:max-w-[250px] md:max-w-[220px] sm:max-w-[100px] md:text-2xl sm:text-xl">
               {interview.jobTitle}
             </p>
 
@@ -49,8 +70,10 @@ export default function InterviewDisplayCard({ index, interview }) {
         </TooltipTrigger>
         <TooltipContent className="max-w-[400px] bg-gray-800 text-white border-gray-700">
           <div className="p-2">
-            <h3 className="text-sm mb-2">{interview.jobTitle || ""}</h3>
-            {/* <div className="text-sm text-gray-300">{interview.jobTitle || ""}</div> */}
+            <h3 className="text-sm mb-2">Job Title:{interview.jobTitle || ""}</h3>
+            <div className="text-sm text-gray-300">Max Score:{interviewOverview.maxScore || "0"}</div>
+            <div className="text-sm text-gray-300">Total Sessions:{interviewOverview?.total || 0}</div>
+            <div className="text-sm text-gray-300">Completed Sessions:{interviewOverview?.totalCompletedInterviews || 0}</div>
           </div>
         </TooltipContent>
       </Tooltip>
