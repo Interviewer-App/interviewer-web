11eb037789c47924c072a6f996d83cd0425d1811
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-02-07 14:17:15 +0530

    refactor: enhance interview flow

--- src/app/interview-room-analiyzer/[id]/interview-room-analizer-dashboard.js
+++ src/app/interview-room-analiyzer/[id]/interview-room-analizer-dashboard.js
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import {
   ResizableHandle,
   ResizablePanel,
@@ -34,6 +34,8 @@ import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 import { RiInformation2Line } from "react-icons/ri";
 import CirculerProgress from "@/components/interview-room-analiyzer/circuler-progress";
 import { StreamVideoCall } from "@/components/video/StreamVideoCall";
+import { useState } from "react";
+import InterviewRoomAnalizerOther from "./interview-room-analizer-other";
 
 const InterviewRoomAnalizerDashboard = forwardRef(
   (
@@ -46,10 +48,26 @@ const InterviewRoomAnalizerDashboard = forwardRef(
       setAnaliyzeResponse,
       typingAnswer,
       setTypingAnswer,
+      categoryScores,
+      setCategoryScores,
     },
     ref
   ) => {
     const videoCallRef = useRef();
+    const [allAnswered, setAllAnswered] = useState(false);
+    const [isAllUnansweredOrNoneAnswered, setIsAllUnansweredOrNoneAnswered] =
+      useState(true);
+
+    useEffect(() => {
+      const unansweredQuestions = questionList.filter(
+        (question) => question.isAnswered === false
+      );
+      const answeredCount = questionList.length - unansweredQuestions;
+      setAllAnswered(unansweredQuestions.length === 0);
+      setIsAllUnansweredOrNoneAnswered(
+        answeredCount === 0 || unansweredQuestions.length === 0
+      );
+    }, [questionList]);
 
     const handleExternalEndCall = () => {
       videoCallRef.current?.endCall();
@@ -58,6 +76,7 @@ const InterviewRoomAnalizerDashboard = forwardRef(
     useImperativeHandle(ref, () => ({
       endCall: handleExternalEndCall,
     }));
+
     const nextQuestion = () => {
       const data = {
         sessionId: sessionId,
@@ -65,6 +84,13 @@ const InterviewRoomAnalizerDashboard = forwardRef(
       socket.emit("nextQuestion", data);
       setAnaliyzeResponse({});
       setTypingAnswer("typing...");
+    };
+
+    const startTechnicalQuestions = () => {
+      const data = {
+        sessionId: sessionId,
+      };
+      socket.emit("startTest", data);
     };
 
     const handleFollowUpQuestion = (question) => {
@@ -82,13 +108,37 @@ const InterviewRoomAnalizerDashboard = forwardRef(
             <ResizablePanelGroup direction="vertical">
               <ResizablePanel defaultSize={60}>
                 <div className=" h-full w-full overflow-y-auto p-6 relative">
-                  <button
-                    variant="secondary"
-                    className=" absolute top-0 right-6 mt-6 bg-white rounded-lg text-center text-sm text-black font-semibold h-11 w-[130px]"
-                    onClick={nextQuestion}
-                  >
-                    Next Question
-                  </button>
+                  {allAnswered ? (
+                    <button
+                      variant="secondary"
+                      className="absolute top-0 right-6 mt-6 bg-white rounded-lg text-center text-sm text-black font-semibold h-11 w-[150px]"
+                      onClick={nextQuestion}
+                    >
+                      End Technical Test
+                    </button>
+                  ) : (
+                    <>
+                      {questionList.filter(
+                        (question) => question.isAnswered === true
+                      ).length === 0 ? (
+                        <button
+                          variant="secondary"
+                          className="absolute top-0 right-6 mt-6 bg-white rounded-lg text-center text-sm text-black font-semibold h-11 w-[150px]"
+                          onClick={startTechnicalQuestions}
+                        >
+                          Start Technical Test
+                        </button>
+                      ) : (
+                        <button
+                          variant="secondary"
+                          className="absolute top-0 right-6 mt-6 bg-white rounded-lg text-center text-sm text-black font-semibold h-11 w-[130px]"
+                          onClick={nextQuestion}
+                        >
+                          Next Question
+                        </button>
+                      )}
+                    </>
+                  )}
                   <h1 className=" text-3xl font-semibold">Question List</h1>
                   <div className=" bg-[#b378ff]/20 mt-5 text-gray-400 border-2 border-[#b378ff] py-2 px-4 rounded-lg flex items-center justify-between">
                     <div className=" mr-9 text-justify text-sm pt-3">
@@ -153,125 +203,142 @@ const InterviewRoomAnalizerDashboard = forwardRef(
           </ResizablePanel>
           <ResizableHandle />
           <ResizablePanel defaultSize={140}>
-            <div className=" h-full overflow-y-auto p-6 overflow-x-hidden relative">
-              <div className=" absolute top-4 right-4 w-[100px] h-[100px]">
-                <CirculerProgress
-                  marks={analiyzeResponse.relevanceScore}
-                  catorgory="Score"
-                  titleSize="text-lg"
-                  subTitleSize="text-[10px]"
+            {isAllUnansweredOrNoneAnswered ? (
+              <div className=" h-full overflow-y-auto p-6 overflow-x-hidden">
+                <InterviewRoomAnalizerOther
+                  categoryScores={categoryScores}
+                  setCategoryScores={setCategoryScores}
+                  sessionId={sessionId}
+                  allocation={false}
                 />
               </div>
-              <h1 className=" text-3xl font-semibold">Live Analysis Result</h1>
-              <div className=" w-full mt-2">
-                <h1 className=" py-3 font-semibold text-lg">Question</h1>
-                <p className=" text-sm text-gray-400">
-                  {candidateAnswers?.question}
-                </p>
-              </div>
-              <div className=" w-full bg-blue-700/20 mt-5 text-gray-400 border-2 border-blue-700 px-4 py-3 rounded-lg">
-                <h1 className=" font-semibold text-lg">
-                  Candidate&apos;s Answer
+            ) : (
+              <div className=" h-full overflow-y-auto p-6 overflow-x-hidden relative">
+                <div className=" absolute top-4 right-4 w-[100px] h-[100px]">
+                  <CirculerProgress
+                    marks={analiyzeResponse.relevanceScore}
+                    catorgory="Score"
+                    titleSize="text-lg"
+                    subTitleSize="text-[10px]"
+                  />
+                </div>
+                <h1 className=" text-3xl font-semibold">
+                  Live Analysis Result
                 </h1>
-                <p className=" text-sm text-gray-400">
-                  {candidateAnswers?.answer}
-                </p>
-              </div>
-              <div className=" w-full bg-gray-700/20 mt-5 text-gray-400 border-2 border-gray-700 rounded-lg py-4 px-7">
-                <h1 className=" text-2xl font-semibold">Analysis with AI</h1>
-                <div className=" mt-2">
-                  <div className=" w-full">
-                    <h1 className="py-3 font-semibold text-lg">keyStrengths</h1>
-                    <ul className="list-disc list-inside text-sm text-gray-400">
-                      {analiyzeResponse.keyStrengths?.map((key, index) => (
-                        <li key={index}>{key}</li>
-                      )) || "No key strengths found"}
-                    </ul>
-                  </div>
-                  <div className=" w-full mt-2">
-                    <h1 className="py-3 font-semibold text-lg">
-                      Areas of Improvement
-                    </h1>
-                    <ul className="list-disc list-inside text-sm text-gray-400">
-                      {analiyzeResponse.areasOfImprovement?.map(
-                        (key, index) => <li key={index}>{key}</li>
-                      ) || "No areas of improvement found"}
-                    </ul>
-                  </div>
-                  <div className=" w-full mt-2">
-                    <h1 className="py-3 font-semibold text-lg">Alignment</h1>
-                    <p className=" text-sm text-gray-400">
-                      {analiyzeResponse?.alignment || ""}
-                    </p>
-                  </div>
+                <div className=" w-full mt-2">
+                  <h1 className=" py-3 font-semibold text-lg">Question</h1>
+                  <p className=" text-sm text-gray-400">
+                    {candidateAnswers?.question}
+                  </p>
                 </div>
-              </div>
-              <div className=" w-full bg-gray-700/20 mt-5 text-gray-400 border-2 border-gray-700 rounded-lg py-4 px-7">
-                <h1 className=" font-semibold text-lg">Available Question</h1>
-                <p className=" text-sm text-gray-400">
-                  {availableQuestion?.questionText || ""}
-                </p>
-              </div>
-              <div className="w-full bg-gray-700/20 mt-5 text-gray-400 border-2 border-gray-700 rounded-lg py-4 px-7">
-                <div>
-                  <h1 className=" text-lg font-semibold">
-                    Follow Up Questions
+                <div className=" w-full bg-blue-700/20 mt-5 text-gray-400 border-2 border-blue-700 px-4 py-3 rounded-lg">
+                  <h1 className=" font-semibold text-lg">
+                    Candidate&apos;s Answer
                   </h1>
-                  <div className=" mt-3 rounded-lg">
-                    {analiyzeResponse.followUpQuestions?.map(
-                      (question, index) => (
-                        <div
-                          key={index}
-                          className=" py-3 text-sm bg-gray-700/20 mt-5 text-gray-400 border-2 border-gray-700 px-4 rounded-lg flex items-center justify-between"
-                        >
-                          <div className=" pr-2">{question}</div>
-                          <div>
-                            <AlertDialog>
-                              <AlertDialogTrigger>
-                                <TooltipProvider>
-                                  <Tooltip>
-                                    <TooltipTrigger asChild>
-                                      <IoMdAddCircleOutline className="text-blue-500 text-[22px] cursor-pointer" />
-                                    </TooltipTrigger>
-                                    <TooltipContent>
-                                      <p>Add as Next Question</p>
-                                    </TooltipContent>
-                                  </Tooltip>
-                                </TooltipProvider>
-                              </AlertDialogTrigger>
+                  <p className=" text-sm text-gray-400">
+                    {candidateAnswers?.answer}
+                  </p>
+                </div>
+                <div className=" w-full bg-gray-700/20 mt-5 text-gray-400 border-2 border-gray-700 rounded-lg py-4 px-7">
+                  <h1 className=" text-2xl font-semibold">Analysis with AI</h1>
+                  <div className=" mt-2">
+                    <div className=" w-full">
+                      <h1 className="py-3 font-semibold text-lg">
+                        keyStrengths
+                      </h1>
+                      <ul className="list-disc list-inside text-sm text-gray-400">
+                        {analiyzeResponse.keyStrengths?.map((key, index) => (
+                          <li key={index}>{key}</li>
+                        )) || "No key strengths found"}
+                      </ul>
+                    </div>
+                    <div className=" w-full mt-2">
+                      <h1 className="py-3 font-semibold text-lg">
+                        Areas of Improvement
+                      </h1>
+                      <ul className="list-disc list-inside text-sm text-gray-400">
+                        {analiyzeResponse.areasOfImprovement?.map(
+                          (key, index) => <li key={index}>{key}</li>
+                        ) || "No areas of improvement found"}
+                      </ul>
+                    </div>
+                    <div className=" w-full mt-2">
+                      <h1 className="py-3 font-semibold text-lg">Alignment</h1>
+                      <p className=" text-sm text-gray-400">
+                        {analiyzeResponse?.alignment || ""}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className=" w-full bg-gray-700/20 mt-5 text-gray-400 border-2 border-gray-700 rounded-lg py-4 px-7">
+                  <h1 className=" font-semibold text-lg">Available Question</h1>
+                  <p className=" text-sm text-gray-400">
+                    {availableQuestion?.questionText || ""}
+                  </p>
+                </div>
+                <div className="w-full bg-gray-700/20 mt-5 text-gray-400 border-2 border-gray-700 rounded-lg py-4 px-7">
+                  <div>
+                    <h1 className=" text-lg font-semibold">
+                      Follow Up Questions
+                    </h1>
+                    <div className=" mt-3 rounded-lg">
+                      {analiyzeResponse.followUpQuestions?.map(
+                        (question, index) => (
+                          <div
+                            key={index}
+                            className=" py-3 text-sm bg-gray-700/20 mt-5 text-gray-400 border-2 border-gray-700 px-4 rounded-lg flex items-center justify-between"
+                          >
+                            <div className=" pr-2">{question}</div>
+                            <div>
+                              <AlertDialog>
+                                <AlertDialogTrigger>
+                                  <TooltipProvider>
+                                    <Tooltip>
+                                      <TooltipTrigger asChild>
+                                        <IoMdAddCircleOutline className="text-blue-500 text-[22px] cursor-pointer" />
+                                      </TooltipTrigger>
+                                      <TooltipContent>
+                                        <p>Add as Next Question</p>
+                                      </TooltipContent>
+                                    </Tooltip>
+                                  </TooltipProvider>
+                                </AlertDialogTrigger>
 
-                              <AlertDialogContent>
-                                <AlertDialogHeader>
-                                  <AlertDialogTitle>
-                                    Are you sure you want to add as next
-                                    question?
-                                  </AlertDialogTitle>
-                                  <AlertDialogDescription>
-                                    This action cannot be undone.
-                                  </AlertDialogDescription>
-                                </AlertDialogHeader>
+                                <AlertDialogContent>
+                                  <AlertDialogHeader>
+                                    <AlertDialogTitle>
+                                      Are you sure you want to add as next
+                                      question?
+                                    </AlertDialogTitle>
+                                    <AlertDialogDescription>
+                                      This action cannot be undone.
+                                    </AlertDialogDescription>
+                                  </AlertDialogHeader>
 
-                                <AlertDialogFooter>
-                                  <AlertDialogCancel>Cancel</AlertDialogCancel>
-                                  <AlertDialogAction
-                                    onClick={() => {
-                                      handleFollowUpQuestion(question);
-                                    }}
-                                    className="h-[40px] font-medium"
-                                  >
-                                    Add
-                                  </AlertDialogAction>
-                                </AlertDialogFooter>
-                              </AlertDialogContent>
-                            </AlertDialog>
+                                  <AlertDialogFooter>
+                                    <AlertDialogCancel>
+                                      Cancel
+                                    </AlertDialogCancel>
+                                    <AlertDialogAction
+                                      onClick={() => {
+                                        handleFollowUpQuestion(question);
+                                      }}
+                                      className="h-[40px] font-medium"
+                                    >
+                                      Add
+                                    </AlertDialogAction>
+                                  </AlertDialogFooter>
+                                </AlertDialogContent>
+                              </AlertDialog>
+                            </div>
                           </div>
-                        </div>
-                      )
-                    ) || "No follow up questions found"}
+                        )
+                      ) || "No follow up questions found"}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            )}
           </ResizablePanel>
         </ResizablePanelGroup>
       </div>
