accfedee0dd875c8d4e2ed3a348ae166db3d5dfb
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-02-12 13:50:44 +0530

    refactor: improve layout and styling of CodeEditor component

--- src/components/CodeEditor/CodeEditor.js
+++ src/components/CodeEditor/CodeEditor.js
@@ -69,21 +69,21 @@ function CodeEditor({
   };
 
   return (
-    <div className="h-lvh w-full px-12 bg-black">
+    <div className="h-lvh w-[80%] max-w-[1500px] mx-auto py-9 bg-black">
       <ResizablePanelGroup
         direction="vertical"
         className=" relative min-h-[calc-200vh-4rem-1px] bg-black text-white mx-auto"
       >
-        <div className=" absolute top-6 right-4 text-gray-400 text-2xl font-semibold">
-        <span className=" text-sm font-normal">Time Remaining</span>{" "}
+        <div className=" absolute top-1 right-4 text-gray-400 text-2xl font-semibold">
+          <span className=" text-sm font-normal">Time Remaining</span>{" "}
           <p className=" text-right">{time}</p>
         </div>
         {/* QUESTION SECTION */}
         <ResizablePanel
           defaultSize={30}
-          className="flex flex-col justify-center items-center"
+          className="flex flex-col justify-center items-center !rounded-lg"
         >
-          <Card className="w-full mx-auto max-w-[1500px]">
+          <Card className="w-full py-8  px-6 !bg-neutral-900 !rounded-lg">
             {" "}
             {/* Add w-[80%] here */}
             <CardHeader className="flex flex-row items-center gap-2">
@@ -101,77 +101,89 @@ function CodeEditor({
         {/* <ResizableHandle withHandle className='!bg-black' /> */}
 
         {/* CODE EDITOR */}
-        <ResizablePanel defaultSize={70} maxSize={80} className="">
-          <div className="w-full max-w-[1500px] flex justify-end mt-36 mb-5 mx-auto">
-            <Select value={language} onValueChange={handleLanguageChange}>
-              <SelectTrigger className="w-[150px]">
-                {/* SELECT VALUE */}
-                <SelectValue>
-                  <div className="flex items-center gap-2">
-                    <Image
-                      width={25}
-                      height={25}
-                      src={LANGUAGES.find((l) => l.id === language)?.icon}
-                      alt={language}
-                      className="w-5 h-5 object-contain"
-                    />
-                    {LANGUAGES.find((l) => l.id === language)?.name}
-                  </div>
-                </SelectValue>
-              </SelectTrigger>
-              <SelectContent>
-                {LANGUAGES.map((lang) => (
-                  <SelectItem key={lang.id} value={lang.id}>
-                    <div className="flex items-center gap-2">
-                      <Image
-                        width={25}
-                        height={25}
-                        src={`${lang.icon}`}
-                        alt={lang.name}
-                        className="w-5 h-5 object-contain"
-                      />
-                      {lang.name}
-                    </div>
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
-          </div>
-          <Editor
-            className="max-w-[1500px] mx-auto  w-full max-h-96"
-            defaultLanguage={language}
-            language={language}
-            theme="vs-dark"
-            value={code}
-            onChange={handleCodeValue}
-            options={{
-              minimap: { enabled: false },
-              fontSize: 18,
-              lineNumbers: "on",
-              scrollBeyondLastLine: false,
-              automaticLayout: true,
-              padding: { top: 16, bottom: 16 },
-              wordWrap: "on",
-              wrappingIndent: "indent",
-            }}
-          />
+        <ResizablePanel defaultSize={70} className="!rounded-lg">
+          {isSubmitBtnAvailable ? (
+            <>
+              <div className="w-full max-w-[1500px] flex justify-end mb-5 mt-10 mx-auto !rounded-lg">
+                <Select value={language} onValueChange={handleLanguageChange}>
+                  <SelectTrigger className="w-[150px]">
+                    {/* SELECT VALUE */}
+                    <SelectValue>
+                      <div className="flex items-center gap-2">
+                        <Image
+                          width={25}
+                          height={25}
+                          src={LANGUAGES.find((l) => l.id === language)?.icon}
+                          alt={language}
+                          className="w-5 h-5 object-contain"
+                        />
+                        {LANGUAGES.find((l) => l.id === language)?.name}
+                      </div>
+                    </SelectValue>
+                  </SelectTrigger>
+                  <SelectContent>
+                    {LANGUAGES.map((lang) => (
+                      <SelectItem key={lang.id} value={lang.id}>
+                        <div className="flex items-center gap-2">
+                          <Image
+                            width={25}
+                            height={25}
+                            src={`${lang.icon}`}
+                            alt={lang.name}
+                            className="w-5 h-5 object-contain"
+                          />
+                          {lang.name}
+                        </div>
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+              <div>
+                <Editor
+                  className="max-w-[1500px] min-h-[400px] mx-auto w-full !rounded-lg max-h-96"
+                  defaultLanguage={language}
+                  language={language}
+                  theme="vs-dark"
+                  value={code}
+                  onChange={handleCodeValue}
+                  options={{
+                    minimap: { enabled: false },
+                    fontSize: 18,
+                    lineNumbers: "on",
+                    scrollBeyondLastLine: false,
+                    automaticLayout: true,
+                    padding: { top: 16, bottom: 16 },
+                    wordWrap: "on",
+                    wrappingIndent: "indent",
+                  }}
+                />
+              </div>
+            </>
+          ) : (
+            <div className=" flex justify-center flex-col items-center h-full">
+              <p className=" text-xl font-semibold">
+                Your answer has been submitted.
+              </p>
+              <p className=" text-gray-500 text-xs">Analyzing your answer...</p>
+            </div>
+          )}
         </ResizablePanel>
 
-        {/* <ResizableHandle withHandle /> */}
-        {isSubmitBtnAvailable && (
-          <div>
-            <div
-              className={`max-w-[1500px] flex justify-center mb-20 mx-auto md:px-12 sm:px-12 `}
+        <div
+          className={`max-w-[1500px] flex justify-center mx-auto md:px-12 sm:px-12 `}
+        >
+          {isSubmitBtnAvailable && (
+            <button
+              onClick={submitCode}
+              className="mt-5 bg-blue-400 hover:bg-blue-500 text-white py-2 px-6 rounded-lg "
             >
-              <button
-                onClick={submitCode}
-                className="mt-5 bg-blue-400 hover:bg-blue-500 text-white py-2 px-6 rounded-lg "
-              >
-                Submit Answer
-              </button>
-            </div>
-          </div>
-        )}
+              Submit Answer
+            </button>
+          )}
+        </div>
+
+        {/* <ResizableHandle withHandle /> */}
       </ResizablePanelGroup>
     </div>
   );
