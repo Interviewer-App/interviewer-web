ad6068ea194077f81ea5fbd269a7da23fe8a7f2d
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-02-28 16:21:34 +0530

    feat: add Bohemian Soul font to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,6 +55,9 @@ module.exports = {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out'
 			},
+			fontFamily:{
+				'bohemian-soul': ['var(--font-bohemian-soul)', 'sans-serif']
+			}
 		
 		}
 	},
