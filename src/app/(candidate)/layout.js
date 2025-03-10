120110bfa82dbffa5183a3cb2b5eb30fbbc8d725
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-03-10 16:33:37 +0530

    fix: wrap SessionValidation children in a div with dark class for improved styling

--- src/app/(candidate)/layout.js
+++ src/app/(candidate)/layout.js
@@ -1,18 +1,16 @@
-import { SidebarProvider } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
-import SessionValidation from "@/context/SessionContext"
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import SessionValidation from "@/context/SessionContext";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
 export default function CandidateLayout({ children }) {
-
   return (
     <>
       <AppRouterCacheProvider>
-     <SessionValidation>
-              {children}
-      </SessionValidation>
+        <div className="dark">
+          <SessionValidation>{children}</SessionValidation>
+        </div>
       </AppRouterCacheProvider>
     </>
-  )
+  );
 }
-
