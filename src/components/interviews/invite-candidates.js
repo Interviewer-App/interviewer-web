90c903e6f77ccb701ce576442ecedbd30745da34
Induwara Sirimanna <146844863+bhashanasirimanna@users.noreply.github.com>
2025-03-24 13:48:17 +0530

    refactor InvitedCandidates component for improved readability and maintainability

--- src/components/interviews/invite-candidates.js
+++ src/components/interviews/invite-candidates.js
@@ -1,20 +1,20 @@
 "use client";
 import { useRouter } from "next/navigation";
 import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
 } from "@/components/ui/tooltip";
 import {
-    Pagination,
-    PaginationContent,
-    PaginationEllipsis,
-    PaginationItem,
-    PaginationLink,
-    PaginationNext,
-    PaginationPrevious,
-  } from "@/components/ui/pagination";
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 import { useEffect, useState } from "react";
 import { useToast } from "@/hooks/use-toast";
 import { ToastAction } from "@/components/ui/toast";
@@ -36,96 +36,98 @@ import { columns } from "@/components/ui/InterviewInvitaionDataTable/column";
 import { getSession } from "next-auth/react";
 import { fetchSendInterviewInvitations } from "@/lib/api/interview-invitation";
 
-export default function InvitedCandidates({interviewId , inviteModalOpen}) {
-    const [loading, setLoading] = useState(false);
-    const [sendInterviewsDataSort, setSendInterviewsDataSort] = useState([]);
-    const [page, setPage] = useState(1);  // Page number
-    const [limit, setLimit] = useState(10);  // Limit of items per page
-    const [totalInvitations, setTotalInvitations] = useState(0);  // Total users count for pagination
-    const [sendInterviewsData, setSendInterviewsData] = useState([]); 
+export default function InvitedCandidates({ interviewId, inviteModalOpen }) {
+  const [loading, setLoading] = useState(false);
+  const [sendInterviewsDataSort, setSendInterviewsDataSort] = useState([]);
+  const [page, setPage] = useState(1); // Page number
+  const [limit, setLimit] = useState(10); // Limit of items per page
+  const [totalInvitations, setTotalInvitations] = useState(0); // Total users count for pagination
+  const [sendInterviewsData, setSendInterviewsData] = useState([]);
 
-    useEffect(() => {
-        const fetchSendInterviews = async () => {
-            try {
-                setLoading(true); // Start loading
-                // const session = await getSession();
-                // const candidateId = session?.user?.candidateID;
-                // console.log('candidate ID:', candidateId);
+  useEffect(() => {
+    const fetchSendInterviews = async () => {
+      try {
+        setLoading(true); // Start loading
+        // const session = await getSession();
+        // const candidateId = session?.user?.candidateID;
+        // console.log('candidate ID:', candidateId);
 
-                // Fetch interviews data
-                await fetchSendInterviewInvitations(interviewId, page, limit, setLoading, setSendInterviewsData, setTotalInvitations);
-            } catch (error) {
-                console.error('Error fetching interviews:', error);
-            }
-        };
-
-        fetchSendInterviews();
-    }, [inviteModalOpen, page, limit]); // Fetch new interviews when page or limit change
-
-    // Log the fetched interviews when payments state changes
-    useEffect(() => {
-        console.log('Fetched interviews:', sendInterviewsData);
-    }, [sendInterviewsData]); // This effect runs whenever payments state changes
-
-    // Pagination handlijgs
-    const handleNextPage = () => {
-        if (page < Math.ceil(totalInvitations / limit)) {
-            setPage(page + 1);
-        }
-    };
-
-    const handlePreviousPage = () => {
-        if (page > 1) {
-            setPage(page - 1);
-        }
-    };
-
-    const handlePage = (newPage) => {
-        if (newPage <= Math.ceil(totalInvitations / limit)) {
-          setPage(newPage);
-        }
+        // Fetch interviews data
+        await fetchSendInterviewInvitations(
+          interviewId,
+          page,
+          limit,
+          setLoading,
+          setSendInterviewsData,
+          setTotalInvitations
+        );
+      } catch (error) {
+        console.error("Error fetching interviews:", error);
       }
+    };
 
+    fetchSendInterviews();
+  }, [inviteModalOpen, page, limit]); // Fetch new interviews when page or limit change
 
-    
-    return (
-        <div className=" bg-slate-600/10 w-full h-fit p-9 rounded-lg mt-5">
-            <div>
-                <h1 className=" text-2xl font-semibold">Interview invitations</h1>
-                <div>
-                    {loading ? (
-                        <div>Loading interview invitations...</div>
-                    ) : (
-                        <DataTable
-                            columns={columns}
-                            data={sendInterviewsData}
-                        />
-                    )}
-                </div>
-            </div>
-            <Pagination>
-                <PaginationContent>
-                    <PaginationItem>
-                        <PaginationPrevious onClick={() => handlePreviousPage()} />
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationLink onClick={() => handlePage(page)}>
-                            {page + 1}
-                        </PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationLink onClick={() => handlePage(page + 1)}>
-                            {page + 2}
-                        </PaginationLink>
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationEllipsis />
-                    </PaginationItem>
-                    <PaginationItem>
-                        <PaginationNext onClick={() => handleNextPage()} />
-                    </PaginationItem>
-                </PaginationContent>
-            </Pagination>
+  // Log the fetched interviews when payments state changes
+  useEffect(() => {
+    console.log("Fetched interviews:", sendInterviewsData);
+  }, [sendInterviewsData]); // This effect runs whenever payments state changes
+
+  // Pagination handlijgs
+  const handleNextPage = () => {
+    if (page < Math.ceil(totalInvitations / limit)) {
+      setPage(page + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const handlePage = (newPage) => {
+    if (newPage <= Math.ceil(totalInvitations / limit)) {
+      setPage(newPage);
+    }
+  };
+
+  return (
+    <div className="w-full h-fit rounded-lg mt-5">
+      <div>
+        <h1 className=" text-2xl font-semibold">Interview invitations</h1>
+        <div>
+          {loading ? (
+            <div>Loading interview invitations...</div>
+          ) : (
+            <DataTable columns={columns} data={sendInterviewsData} />
+          )}
         </div>
-    );
+      </div>
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious onClick={() => handlePreviousPage()} />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink onClick={() => handlePage(page)}>
+              {page + 1}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink onClick={() => handlePage(page + 1)}>
+              {page + 2}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext onClick={() => handleNextPage()} />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
 }
