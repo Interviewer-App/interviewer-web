208ce1f4fc78adeee2467589d245195a99a0a947
Madhawa Awishka <madhawaawishka@gmail.com>
2025-03-24 13:28:55 +0530

    enhance CreateQuestionModal UI with improved styling and additional labels

--- src/components/company/create-question-modal.js
+++ src/components/company/create-question-modal.js
@@ -89,10 +89,15 @@ export default function CreateQuestionModal({ forSession, id, setCreateModalOpen
 
   return (
     <div className=" fixed  top-0 left-0 z-50 h-full w-full flex items-center justify-center bg-black/50">
-      <div className=" relative max-w-[700px] h-fit max-h-[670px] w-[90%] md:w-[50%] p-9 bg-gradient-to-br from-[#1f2126] to-[#17191d] rounded-lg">
-        <h1 className=" text-2xl font-semibold text-[#f3f3f3] pb-5">
-          Create Question
-        </h1>
+      <div className=" relative max-w-[700px] h-fit max-h-[670px] w-[90%] md:w-[50%] p-9 bg-[#09090b] rounded-lg border border-[#2d2f36]">
+        <div className="pb-5">
+          <h1 className=" text-2xl font-semibold text-[#f3f3f3] pb-1 flex items-center gap-2">
+            Create Questions
+          </h1>
+          <span className="text-[#737883] text-sm !pb-5">
+            Create a new technical question for the interview.
+          </span>
+        </div>
         <button
           onClick={() => setCreateModalOpen(false)}
           className=" absolute top-5 right-5 text-[#f3f3f3]"
@@ -100,17 +105,23 @@ export default function CreateQuestionModal({ forSession, id, setCreateModalOpen
           <MdClose className=" text-2xl" />
         </button>
         <form onSubmit={handleSubmit}>
+          <label className="block text-sm font-medium text-[#f3f3f3] mb-2">
+            Question
+          </label>
           <textarea
-            placeholder="Question"
+            placeholder="e.g. What is the difference between a stack and a queue?"
             name="question"
             value={question}
             onChange={(e) => setQuestion(e.target.value)}
             required
-            className=" h-[150px] w-full rounded-lg text-sm border-0 bg-[#32353b] placeholder-[#737883] px-6 py-2"
+            className=" h-[150px] w-full rounded-lg text-sm border bg-[#09090b] border-[#2d2f36] placeholder-[#737883] px-6 py-2"
           />
 
           <div className=" w-full grid grid-cols-1 md:grid-cols-2 gap-2  mt-5">
             <div className="w-full">
+              <label className="block text-sm font-medium text-[#f3f3f3] mb-2">
+                Duration(minutes)
+              </label>
               <input
                 type="number"
                 placeholder="Estimated Time (Minutes)"
@@ -118,31 +129,37 @@ export default function CreateQuestionModal({ forSession, id, setCreateModalOpen
                 value={time}
                 onChange={(e) => setTime(e.target.value)}
                 required
-                className=" h-[45px] w-full rounded-lg text-sm border-0 bg-[#32353b] placeholder-[#737883] px-6 py-2 mb-5"
+                className=" h-[45px] w-full rounded-lg text-sm border bg-[#09090b] border-[#2d2f36] placeholder-[#737883] px-6 py-2 mb-5"
               />
             </div>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  className={`!bg-[#32353b] w-full h-[45px] m-0 px-2 focus:outline-none outline-none`}
-                  variant="outline"
-                >
-                  {type === "CODING" ? "Coding" : "Open Ended"}
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56">
-                <DropdownMenuLabel>Question Type</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuRadioGroup value={type} onValueChange={setType}>
-                  <DropdownMenuRadioItem value="OPEN_ENDED">
-                    Open Ended
-                  </DropdownMenuRadioItem>
-                  <DropdownMenuRadioItem value="CODING">
-                    Coding
-                  </DropdownMenuRadioItem>
-                </DropdownMenuRadioGroup>
-              </DropdownMenuContent>
-            </DropdownMenu>
+
+            <div>
+              <label className="block text-sm font-medium text-[#f3f3f3] mb-2">
+                Category
+              </label>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    className={`bg-[#09090b] border-[#2d2f36] w-full h-[45px] m-0 px-2 focus:outline-none outline-none`}
+                    variant="outline" 
+                  >
+                    {type === "CODING" ? "Coding" : "Open Ended"}
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56">
+                  <DropdownMenuLabel>Question Type</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuRadioGroup value={type} onValueChange={setType}>
+                    <DropdownMenuRadioItem value="OPEN_ENDED">
+                      Open Ended
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="CODING">
+                      Coding
+                    </DropdownMenuRadioItem>
+                  </DropdownMenuRadioGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
           </div>
           <div className=" w-full flex flex-col md:flex-row justify-between items-center mt-1"></div>
 
