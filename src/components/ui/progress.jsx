3938d847632a277fd7c409c79455ec3e7f97d1fe
bhashanasirimanna <bhashanasirimanna@gmail.com>
2025-04-10 15:19:16 +0530

    Add indicator class support to Progress component

--- src/components/ui/progress.jsx
+++ src/components/ui/progress.jsx
@@ -5,7 +5,7 @@ import * as ProgressPrimitive from "@radix-ui/react-progress"
 
 import { cn } from "@/lib/utils"
 
-const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
+const Progress = React.forwardRef(({ className, value, indicatorclassName, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -14,7 +14,7 @@ const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
     )}
     {...props}>
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-neutral-900 transition-all dark:bg-neutral-50"
+      className={cn( "h-full w-full flex-1 bg-neutral-900 transition-all dark:bg-neutral-50", indicatorclassName)}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }} />
   </ProgressPrimitive.Root>
 ))
